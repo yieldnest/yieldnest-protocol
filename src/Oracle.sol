e8fcf29af9d4a8136754fcdcd87dfdbd47364dd3
danoctavian <danoctavian91@gmail.com>
2023-12-15 05:39:45 +0200

    add skeleton Oracle

---
+++ src/Oracle.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract Oracle is Initializable {
+
+    /*
+      TODO: implement oracle that measures:
+      
+      1. Beacon Chain balances 
+      2. Eigen Layer balances
+    */
+
+    function initialize() public initializer {
+    }
+}
+
