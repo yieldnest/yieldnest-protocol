9f561a942cac4c73344cfa7001a0a8b47a554cde
danoctavian <danoctavian91@gmail.com>
2023-12-28 03:49:21 +0200

    refactor into deploy.js function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,6 +7,41 @@ const { deployAndInitializeTransparentUpgradeableProxy, deployProxy, upgradeProx
 
 
 async function main() {
+
+    const { ynETH, oracle, stakingNodesManager } = await deploy();
+    console.log("Verifying contracts on etherscan");
+    async function retryVerify(contractName, contractAddress, constructorArguments) {
+        while (true) {
+            try {
+                await hre.run("verify:verify", {
+                    address: contractAddress,
+                    constructorArguments: constructorArguments,
+                });
+                console.log(`${contractName} verified successfully`);
+                break;
+            } catch (error) {
+                console.error(`Error verifying ${contractName}, retrying in 10 seconds`, error);
+                await new Promise(resolve => setTimeout(resolve, 10000));
+            }
+        }
+    }
+
+    await retryVerify("ynETH", ynETH.address, []);
+    await retryVerify("Oracle", oracle.address, []);
+    await retryVerify("StakingNodesManager", stakingNodesManager.address, []);
+    console.log("Contracts verified successfully");
+
+    const addresses = {
+        ynETHContract: ynETHContract.address,
+        oracle: oracle.address
+    };
+
+    fs.writeFileSync('goerli-addresses.json', JSON.stringify(addresses, null, 2));
+    console.log("Addresses written to goerli-addresses.json");
+
+}
+
+async function deploy() {
     const [deployer] = await hre.ethers.getSigners();
 
     // set deployer to be the manager 
@@ -94,35 +129,7 @@ async function main() {
 
     console.log("Contracts initialized successfully");
 
-    console.log("Verifying contracts on etherscan");
-    async function retryVerify(contractName, contractAddress, constructorArguments) {
-        while (true) {
-            try {
-                await hre.run("verify:verify", {
-                    address: contractAddress,
-                    constructorArguments: constructorArguments,
-                });
-                console.log(`${contractName} verified successfully`);
-                break;
-            } catch (error) {
-                console.error(`Error verifying ${contractName}, retrying in 10 seconds`, error);
-                await new Promise(resolve => setTimeout(resolve, 10000));
-            }
-        }
-    }
-
-    await retryVerify("ynETHContract", ynETHContract.address, []);
-    await retryVerify("Oracle", oracle.address, []);
-    console.log("Contracts verified successfully");
-
-    const addresses = {
-        ynETHContract: ynETHContract.address,
-        oracle: oracle.address
-    };
-
-    fs.writeFileSync('goerli-addresses.json', JSON.stringify(addresses, null, 2));
-    console.log("Addresses written to goerli-addresses.json");
-
+    return { ynETH, oracle, stakingNodesManager };
 }
 
 main()
