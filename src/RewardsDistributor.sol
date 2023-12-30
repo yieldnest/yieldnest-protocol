3e5356cdfecfff609b7221f46905bb6b407ba460
danoctavian <danoctavian91@gmail.com>
2023-12-30 18:50:00 +0200

    add skeleton contracts for rewards and strategy

---
+++ src/RewardsDistributor.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract RewardsDistributor is Initializable {
+
+    function initialize() public initializer {
+
+    }
+}
---
+++ src/StrategyManager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract StrategyManager is Initializable {
+
+    function initialize() public initializer {
+
+    }
+
+}
