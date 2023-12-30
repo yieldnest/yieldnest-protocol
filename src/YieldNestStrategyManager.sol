7d41a9941815eaeab1a5bf1614c6de641befdaba
danoctavian <danoctavian91@gmail.com>
2023-12-31 01:13:50 +0200

    rename strategy manage

--- src/YieldNestStrategyManager.sol
+++ src/YieldNestStrategyManager.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract StrategyManager is Initializable {
+contract YieldNestStrategyManager is Initializable {
 
     function initialize() public initializer {
 
