06400a75c823edb678627aaf675ecf2123541c61
danoctavian <danoctavian91@gmail.com>
2024-01-12 02:15:15 +0200

    enhance interface

--- src/interfaces/IStakingNodesManager.sol
+++ src/interfaces/IStakingNodesManager.sol
@@ -20,6 +20,11 @@ interface IStakingNodesManager {
     function isStakingNodesAdmin(address) external view returns (bool);
 
     function processWithdrawnETH(uint nodeId) external payable;
+
+    function registerValidators(
+        bytes32 _depositRoot,
+        DepositData[] calldata _depositData
+    ) external;
 }
 
 
