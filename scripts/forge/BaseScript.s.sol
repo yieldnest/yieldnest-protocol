55939293407d64a03e705d4d0c1a3932ea3731fb
danoctavian <danoctavian91@gmail.com>
2024-02-17 02:15:32 +0200

    add deploy script

---
+++ scripts/forge/BaseScript.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "forge-std/Script.sol";
+
+/**
+ * @title Base Script
+ * @author Puffer Finance
+ */
+abstract contract BaseScript is Script {
+
+}
---
+++ scripts/forge/DeployYieldNest.s.sol
@@ -0,0 +1,151 @@
+import "forge-std/Script.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "../../src/StakingNodesManager.sol";
+import "../../src/RewardsReceiver.sol";
+import "../../src/RewardsDistributor.sol";
+import "../../src/external/WETH.sol";
+import "../../src/ynETH.sol";
+import "../../src/interfaces/IStakingNode.sol";
+import "../../src/interfaces/IDepositContract.sol";
+import "../../src/interfaces/IRewardsDistributor.sol";
+import "../../src/interfaces/IWETH.sol";
+import "../../test/foundry/ContractAddresses.sol";
+
+
+contract DeployYieldNest is Script {
+
+    ProxyAdmin public proxyAdmin;
+    TransparentUpgradeableProxy public ynethProxy;
+    TransparentUpgradeableProxy public stakingNodesManagerProxy;
+    TransparentUpgradeableProxy public rewardsDistributorProxy;
+    
+    ynETH public yneth;
+    StakingNodesManager public stakingNodesManager;
+    RewardsReceiver public executionLayerReceiver;
+    RewardsDistributor public rewardsDistributor;
+    StakingNode public stakingNodeImplementation;
+    address payable feeReceiver;
+
+    IEigenPodManager public eigenPodManager;
+    IDelegationManager public delegationManager;
+    IDelayedWithdrawalRouter public delayedWithdrawalRouter;
+    IStrategyManager public strategyManager;
+    IDepositContract public depositContract;
+
+    uint startingExchangeAdjustmentRate;
+
+    bytes ZERO_PUBLIC_KEY = hex"000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"; 
+    bytes ONE_PUBLIC_KEY = hex"000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001";
+
+    bytes ZERO_SIGNATURE = hex"000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+    bytes32 ZERO_DEPOSIT_ROOT = bytes32(0);
+
+    function run() external {
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+
+        // ynETH.sol ROLES
+        address ynethAdminAddress = vm.envAddress("YNETH_ADMIN_ADDRESS");
+        address pauserAddress = vm.envAddress("PAUSER_ADDRESS");
+
+        address rewardsDistributorAdminAddress = vm.envAddress("REWARDS_DISTRIBUTOR_ADMIN_ADDRESS");
+
+        // StakingNodesManager.sol ROLES
+        address stakingNodesManagerAdminAddress = vm.envAddress("STAKING_NODES_MANAGER_ADMIN_ADDRESS");
+        address stakingAdminAddress = vm.envAddress("STAKING_ADMIN_ADDRESS");
+        address stakingNodesAdminAddress = vm.envAddress("STAKING_NODES_ADMIN_ADDRESS");
+        address validatorManagerAddress = vm.envAddress("VALIDATOR_MANAGER_ADDRESS");
+
+        address _broadcaster = vm.addr(deployerPrivateKey);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+
+        console.log("Default Signer Address:", _broadcaster);
+
+
+        feeReceiver = payable(_broadcaster); // Casting the default signer address to payable
+
+
+        startingExchangeAdjustmentRate = 4;
+
+        proxyAdmin = new ProxyAdmin(address(this));
+        WETH weth = new WETH();
+
+        // Deploy implementations
+        yneth = new ynETH();
+        stakingNodesManager = new StakingNodesManager();
+        executionLayerReceiver = new RewardsReceiver();
+        stakingNodeImplementation = new StakingNode();
+
+        RewardsDistributor rewardsDistributorImplementation = new RewardsDistributor();
+        rewardsDistributorProxy = new TransparentUpgradeableProxy(address(rewardsDistributorImplementation), address(proxyAdmin), "");
+        rewardsDistributor = RewardsDistributor(payable(rewardsDistributorProxy));
+
+        // Deploy proxies
+        ynethProxy = new TransparentUpgradeableProxy(address(yneth), address(proxyAdmin), "");
+        stakingNodesManagerProxy = new TransparentUpgradeableProxy(address(stakingNodesManager), address(proxyAdmin), "");
+
+        yneth = ynETH(payable(ynethProxy));
+        stakingNodesManager = StakingNodesManager(payable(stakingNodesManagerProxy));
+
+        // Initialize ynETH with example parameters
+        ynETH.Init memory ynethInit = ynETH.Init({
+            admin: ynethAdminAddress,
+            pauser: pauserAddress,
+            stakingNodesManager: IStakingNodesManager(address(stakingNodesManager)),
+            rewardsDistributor: IRewardsDistributor(address(rewardsDistributor)),
+            wETH: IWETH(address(weth)),  // Deployed WETH address
+            exchangeAdjustmentRate: startingExchangeAdjustmentRate
+        });
+        yneth.initialize(ynethInit);
+
+        ContractAddresses contractAddresses = new ContractAddresses();
+        ContractAddresses.ChainAddresses memory chainAddresses = contractAddresses.getChainAddresses(block.chainid);
+        eigenPodManager = IEigenPodManager(chainAddresses.EIGENLAYER_EIGENPOD_MANAGER_ADDRESS);
+        delegationManager = IDelegationManager(chainAddresses.EIGENLAYER_DELEGATION_MANAGER_ADDRESS);
+        delayedWithdrawalRouter = IDelayedWithdrawalRouter(chainAddresses.EIGENLAYER_DELAYED_WITHDRAWAL_ROUTER_ADDRESS); // Assuming DEPOSIT_2_ADDRESS is used for DelayedWithdrawalRouter
+        strategyManager = IStrategyManager(chainAddresses.EIGENLAYER_STRATEGY_MANAGER_ADDRESS);
+        depositContract = IDepositContract(chainAddresses.DEPOSIT_2_ADDRESS);
+
+        // Initialize StakingNodesManager with example parameters
+        StakingNodesManager.Init memory stakingNodesManagerInit = StakingNodesManager.Init({
+            admin: stakingNodesManagerAdminAddress,
+            stakingAdmin: stakingAdminAddress,
+            stakingNodesAdmin: stakingNodesAdminAddress,
+            validatorManager: validatorManagerAddress,
+            maxNodeCount: 10,
+            depositContract: depositContract,
+            ynETH: IynETH(address(yneth)),
+            eigenPodManager: eigenPodManager,
+            delegationManager: delegationManager,
+            delayedWithdrawalRouter: delayedWithdrawalRouter,
+            strategyManager: strategyManager
+        });
+        stakingNodesManager.initialize(stakingNodesManagerInit);
+
+        stakingNodesManager.registerStakingNodeImplementationContract(address(stakingNodeImplementation));
+
+        RewardsDistributor.Init memory rewardsDistributorInit = RewardsDistributor.Init({
+            admin: rewardsDistributorAdminAddress,
+            executionLayerReceiver: executionLayerReceiver,
+            feesReceiver: feeReceiver, // Assuming the contract itself will receive the fees
+            ynETH: IynETH(address(yneth))
+        });
+        rewardsDistributor.initialize(rewardsDistributorInit);
+
+        // Initialize RewardsReceiver with example parameters
+        RewardsReceiver.Init memory rewardsReceiverInit = RewardsReceiver.Init({
+            admin: rewardsDistributorAdminAddress,
+            withdrawer: address(rewardsDistributor)
+        });
+        executionLayerReceiver.initialize(rewardsReceiverInit);
+
+        vm.stopBroadcast();
+    }
+}
+
+
+
