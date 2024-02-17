56344c3757ad6550a949a5d0b55e3a0c5cb25a9c
danoctavian <danoctavian91@gmail.com>
2024-02-17 15:09:53 +0200

    fix hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,11 +16,11 @@ const networks = {
   },
   goerli: {
     url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-    accounts: [`0x${process.env.PRIVATE_KEY}`]
+    accounts: [`${process.env.PRIVATE_KEY}`]
   },
   mainnet: {
     url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-    accounts: [`0x${process.env.PRIVATE_KEY}`]
+    accounts: [`${process.env.PRIVATE_KEY}`]
   }
 };
 
