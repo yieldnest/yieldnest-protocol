234ed5c50a4a0e25af46afbe06c612d4d06b0088
danoctavian <danoctavian91@gmail.com>
2024-02-20 18:31:39 +0200

    add Utils.sol file with functions for loading proxy impl and admin addresses

---
+++ scripts/forge/Utils.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.0;
+
+import {Vm} from "forge-std/Vm.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+
+contract Utils {
+    /**
+     * @dev Returns the admin address of a TransparentUpgradeableProxy contract.
+     * @param proxy The address of the TransparentUpgradeableProxy.
+     * @return The admin address of the proxy contract.
+     */
+    function getTransparentUpgradeableProxyAdminAddress(address proxy) public view returns (address) {
+        address CHEATCODE_ADDRESS = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
+        Vm vm = Vm(CHEATCODE_ADDRESS);
+
+        bytes32 adminSlot = vm.load(proxy, ERC1967Utils.ADMIN_SLOT);
+        return address(uint160(uint256(adminSlot)));
+    }
+
+    /**
+     * @dev Returns the implementation address of a TransparentUpgradeableProxy contract.
+     * @param proxy The address of the TransparentUpgradeableProxy.
+     * @return The implementation address of the proxy contract.
+     */
+    function getTransparentUpgradeableProxyImplementationAddress(address proxy) public view returns (address) {
+        address CHEATCODE_ADDRESS = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
+        Vm vm = Vm(CHEATCODE_ADDRESS);
+
+        bytes32 implementationSlot = vm.load(proxy, ERC1967Utils.IMPLEMENTATION_SLOT);
+        return address(uint160(uint256(implementationSlot)));
+    }
+}
