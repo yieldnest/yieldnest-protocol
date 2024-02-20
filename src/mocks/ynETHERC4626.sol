b7175e3b6480bac6138ee6e9e1860303f3ee0c4b
danoctavian <danoctavian91@gmail.com>
2024-02-20 20:57:10 +0200

    add test for showing ynETH can be upgraded into a 4626 later

--- src/mocks/ynETHERC4626.sol
+++
@@ -1,4 +0,0 @@
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
-
-contract ynETHERC4626 is ERC4626Upgradeable {
-}
--- test/foundry/integration/Upgrades.t.sol
+++ test/foundry/integration/Upgrades.t.sol
@@ -6,17 +6,18 @@ import "./IntegrationBaseTest.sol";
 import "forge-std/console.sol";
 import "../../../src/StakingNodesManager.sol";
 import "../../../src/ynViewer.sol";
-import "../../../src/ynETH.sol";
 import "../../../src/mocks/MockStakingNode.sol";
-import "../../../src/mocks/ynETHERC4626.sol";
+import "../../../src/mocks/MockYnETHERC4626.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol";
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "../../../src/mocks/MockERC20.sol";
+
 
 contract UpgradesTest is IntegrationBaseTest {
 
     function testUpgradeEachTransparentProxyUpgradeableContract() public {
 
-        address newImplementation = address(new ynETHERC4626()); 
+        address newImplementation = address(new ynETH()); 
         vm.prank(proxyAdmin);
         ProxyAdmin(getTransparentUpgradeableProxyAdminAddress(address(yneth))).upgradeAndCall(ITransparentUpgradeableProxy(address(yneth)), newImplementation, "");
 
@@ -34,5 +35,54 @@ contract UpgradesTest is IntegrationBaseTest {
         ProxyAdmin(getTransparentUpgradeableProxyAdminAddress(address(rewardsDistributor))).upgradeAndCall(ITransparentUpgradeableProxy(address(rewardsDistributor)), newRewardsDistributorImpl, "");
         address currentRewardsDistributorImpl = getTransparentUpgradeableProxyImplementationAddress(address(rewardsDistributor));
         assertEq(currentRewardsDistributorImpl, newRewardsDistributorImpl);
+    }
+
+    function testUpgradeabilityofYnETHToAnERC4626() public {
+
+        uint256 depositAmount = 1 ether;
+        vm.deal(address(this), depositAmount);
+        vm.prank(address(this));
+        yneth.depositETH{value: depositAmount}(address(this));
+
+        uint256 finalTotalAssets = yneth.totalAssets();
+        uint256 finalTotalSupply = yneth.totalSupply();
+        // Save original ynETH state
+        IStakingNodesManager originalStakingNodesManager = yneth.stakingNodesManager();
+        IRewardsDistributor originalRewardsDistributor = yneth.rewardsDistributor();
+        uint originalAllocatedETHForDeposits = yneth.allocatedETHForDeposits();
+        bool originalIsDepositETHPaused = yneth.isDepositETHPaused();
+        uint originalExchangeAdjustmentRate = yneth.exchangeAdjustmentRate();
+        uint originalTotalDepositedInPool = yneth.totalDepositedInPool();
+
+        MockERC20 nETH = new MockERC20("Nest ETH", "nETH");
+        nETH.mint(address(this), 100000 ether);
+
+        address newImplementation = address(new MockYnETHERC4626()); 
+        vm.prank(proxyAdmin);
+        ProxyAdmin(getTransparentUpgradeableProxyAdminAddress(address(yneth)))
+            .upgradeAndCall(ITransparentUpgradeableProxy(
+                address(yneth)),
+                newImplementation,
+                abi.encodeWithSelector(MockYnETHERC4626.reinitialize.selector, MockYnETHERC4626.ReInit({underlyingAsset: nETH})
+                )
+            );
+
+        MockYnETHERC4626 upgradedYnETH = MockYnETHERC4626(payable(address(yneth)));
+
+        // Assert that all fields are equal post-upgrade
+        assertEq(address(upgradedYnETH.stakingNodesManager()), address(originalStakingNodesManager), "StakingNodesManager mismatch");
+        assertEq(address(upgradedYnETH.rewardsDistributor()), address(originalRewardsDistributor), "RewardsDistributor mismatch");
+        assertEq(upgradedYnETH.allocatedETHForDeposits(), originalAllocatedETHForDeposits, "AllocatedETHForDeposits mismatch");
+        assertEq(upgradedYnETH.isDepositETHPaused(), originalIsDepositETHPaused, "IsDepositETHPaused mismatch");
+        assertEq(upgradedYnETH.exchangeAdjustmentRate(), originalExchangeAdjustmentRate, "ExchangeAdjustmentRate mismatch");
+        assertEq(upgradedYnETH.totalDepositedInPool(), originalTotalDepositedInPool, "TotalDepositedInPool mismatch");
+
+        assertEq(finalTotalAssets, yneth.totalAssets(), "Total assets mismatch after upgrade");
+        assertEq(finalTotalSupply, yneth.totalSupply(), "Total supply mismatch after upgrade");
+
+        uint256 nETHDepositAmount = 100 ether;
+        nETH.approve(address(yneth), nETHDepositAmount);
+        vm.prank(address(this));
+        upgradedYnETH.deposit(nETHDepositAmount, address(this));
     }
 }
