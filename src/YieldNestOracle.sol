6d5fecf3f6fd18176ee95d5e3f81f5df73879976
xhad <chyad@hotmail.com>
2024-02-24 15:29:44 -0500

    Lint: YieldNestOracle.sol header cleanup

--- src/YieldNestOracle.sol
+++ src/YieldNestOracle.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
 import { AccessControlUpgradeable } from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
