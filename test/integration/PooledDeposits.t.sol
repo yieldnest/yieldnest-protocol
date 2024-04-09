f418f11efea43f39c3ba722adc7a4510beec7a90
danoctavian <danoctavian91@gmail.com>
2024-04-09 14:56:08 +0300

    add fuzz test for depositor count and amount

--- test/integration/PooledDeposits.t.sol
+++ test/integration/PooledDeposits.t.sol
@@ -40,6 +40,37 @@ contract PooledDepositsTest is IntegrationBaseTest {
         assertEq(yneth.balanceOf(address(this)), expectedYnETHAmount, "ynETH should be minted and sent to the depositor");
     }
 
+    function testFinalizeDepositsFuzz(uint8 depositorsCount, uint256 depositAmount) public {
+        // Fuzz inputs
+        vm.assume(depositorsCount > 0 && depositorsCount <= 100); // Limiting the number of depositors to a reasonable range
+        vm.assume(depositAmount > 0.01 ether && depositAmount <= 100 ether); // Assuming a reasonable range for deposit amounts
+
+        // Arrange
+        PooledDeposits pooledDeposits = new PooledDeposits(IynETH(address(yneth)), block.timestamp + 2 days);
+        address[] memory depositors = new address[](depositorsCount);
+        uint256 totalDepositAmount = 0;
+
+        for (uint8 i = 0; i < depositorsCount; i++) {
+            address depositor = address(uint160(uint(keccak256(abi.encodePacked(i, block.timestamp)))));
+            depositors[i] = depositor;
+            vm.deal(depositor, depositAmount);
+            vm.prank(depositor);
+            pooledDeposits.deposit{value: depositAmount}();
+            totalDepositAmount += depositAmount;
+        }
+
+        // Act
+        vm.warp(block.timestamp + 3 days); // Move time forward to allow finalizing deposits
+        pooledDeposits.finalizeDeposits(depositors);
+
+        // Assert
+        // Assuming ynETH's depositETH function simply mints 1:1 ynETH for ETH deposited
+        for (uint8 i = 0; i < depositorsCount; i++) {
+            uint256 expectedYnETHAmount = depositAmount;
+            assertEq(yneth.balanceOf(depositors[i]), expectedYnETHAmount, "ynETH should be minted and sent to the depositor");
+        }
+    }
+
     receive() external payable {}
 }
 
