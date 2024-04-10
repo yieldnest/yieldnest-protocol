a5b1df8b2da4489155fca2616413f70c5909b5fe
danoctavian <danoctavian91@gmail.com>
2024-04-10 21:25:33 +0300

    tests for roles

--- test/integration/Roles.t.sol
+++ test/integration/Roles.t.sol
@@ -19,23 +19,38 @@ import {TestStakingNodesManagerV2} from "test/mocks/TestStakingNodesManagerV2.so
 
 contract RolesTest is IntegrationBaseTest {
 
-    // function testRoleChangeYnETH() public {
-    //     address newAdmin = address(0x123);
-    //     vm.prank(actors.ADMIN);
-    //     ynETH.updateAdmin(newAdmin);
+    function testRoleChangeYnETH() public {
+        address newOperator = address(0x123);
+        bytes32 ADMIN_ROLE = keccak256("ADMIN_ROLE");
+        bytes32 PAUSER_ROLE = keccak256("PAUSER_ROLE");
+        bytes32 STAKING_NODES_MANAGER_ROLE = keccak256("STAKING_NODES_MANAGER_ROLE");
+        bytes32 REWARDS_DISTRIBUTOR_ROLE = keccak256("REWARDS_DISTRIBUTOR_ROLE");
 
-    //     address currentAdmin = ynETH.admin();
-    //     assertEq(currentAdmin, newAdmin);
-    // }
+        assertTrue(stakingNodesManager.hasRole(stakingNodesManager.DEFAULT_ADMIN_ROLE(), actors.ADMIN));
+        assertTrue(stakingNodesManager.hasRole(PAUSER_ROLE, actors.PAUSE_ADMIN));
+
+        vm.startPrank(actors.ADMIN);
+        yneth.grantRole(PAUSER_ROLE, newOperator);
+        yneth.revokeRole(PAUSER_ROLE, actors.PAUSE_ADMIN);
+        vm.stopPrank();
+
+        assertTrue(yneth.hasRole(PAUSER_ROLE, newOperator));
+    }
 
     function testRoleChangeStakingNodesManager() public {
         address newOperator = address(0x456);
-        bytes32 OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
         bytes32 STAKING_ADMIN_ROLE = keccak256("STAKING_ADMIN_ROLE");
         bytes32 VALIDATOR_MANAGER_ROLE = keccak256("VALIDATOR_MANAGER_ROLE");
         bytes32 STAKING_NODES_ADMIN_ROLE = keccak256("STAKING_NODES_ADMIN_ROLE");
         bytes32 STAKING_NODE_CREATOR_ROLE = keccak256("STAKING_NODE_CREATOR_ROLE");
         bytes32 PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+        assertTrue(stakingNodesManager.hasRole(stakingNodesManager.DEFAULT_ADMIN_ROLE(), actors.ADMIN));
+        assertTrue(stakingNodesManager.hasRole(STAKING_ADMIN_ROLE, actors.STAKING_ADMIN));
+        assertTrue(stakingNodesManager.hasRole(VALIDATOR_MANAGER_ROLE, actors.VALIDATOR_MANAGER));
+        assertTrue(stakingNodesManager.hasRole(STAKING_NODES_ADMIN_ROLE, actors.STAKING_NODES_ADMIN));
+        assertTrue(stakingNodesManager.hasRole(STAKING_NODE_CREATOR_ROLE, actors.STAKING_NODE_CREATOR));
+        assertTrue(stakingNodesManager.hasRole(PAUSER_ROLE, actors.PAUSE_ADMIN));
 
         vm.startPrank(actors.ADMIN);
         stakingNodesManager.grantRole(STAKING_ADMIN_ROLE, newOperator);
@@ -53,49 +68,67 @@ contract RolesTest is IntegrationBaseTest {
         stakingNodesManager.grantRole(PAUSER_ROLE, newOperator);
         stakingNodesManager.revokeRole(PAUSER_ROLE, actors.PAUSE_ADMIN);
         vm.stopPrank();
-        
+
         assertTrue(stakingNodesManager.hasRole(STAKING_ADMIN_ROLE, newOperator));
         assertTrue(stakingNodesManager.hasRole(VALIDATOR_MANAGER_ROLE, newOperator));
         assertTrue(stakingNodesManager.hasRole(STAKING_NODES_ADMIN_ROLE, newOperator));
         assertTrue(stakingNodesManager.hasRole(STAKING_NODE_CREATOR_ROLE, newOperator));
         assertTrue(stakingNodesManager.hasRole(PAUSER_ROLE, newOperator));
+
+        assertFalse(stakingNodesManager.hasRole(STAKING_ADMIN_ROLE, actors.STAKING_ADMIN));
+        assertFalse(stakingNodesManager.hasRole(VALIDATOR_MANAGER_ROLE, actors.VALIDATOR_MANAGER));
+        assertFalse(stakingNodesManager.hasRole(STAKING_NODES_ADMIN_ROLE, actors.STAKING_NODES_ADMIN));
+        assertFalse(stakingNodesManager.hasRole(STAKING_NODE_CREATOR_ROLE, actors.STAKING_NODE_CREATOR));
+        assertFalse(stakingNodesManager.hasRole(PAUSER_ROLE, actors.PAUSE_ADMIN));
     }
 
-    // function testRoleChangeRewardsDistributor() public {
-    //     address newDistributor = address(0x789);
-    //     vm.prank(actors.DISTRIBUTOR);
-    //     RewardsDistributor.updateDistributor(newDistributor);
-        
-    //     address currentDistributor = RewardsDistributor.distributor();
-    //     assertEq(currentDistributor, newDistributor);
-    // }
+    function testRoleChangeRewardsDistributor() public {
+        // TODO: add after fixing roles in RewardsDistributor.sol
+    }
 
-    // function testRoleChangeYnLSD() public {
-    //     address newStrategyManager = address(0xABC);
-    //     vm.prank(actors.STRATEGY_MANAGER);
-    //     ynLSD.updateStrategyManager(newStrategyManager);
-        
-    //     address currentStrategyManager = ynLSD.strategyManager();
-    //     assertEq(currentStrategyManager, newStrategyManager);
-    // }
+    function testRoleChangeYnLSD() public {
+        address newStakingAdmin = address(0xABC);
+        bytes32 STAKING_ADMIN_ROLE = keccak256("STAKING_ADMIN_ROLE");
+        bytes32 LSD_RESTAKING_MANAGER_ROLE = keccak256("LSD_RESTAKING_MANAGER_ROLE");
+        bytes32 LSD_STAKING_NODE_CREATOR_ROLE = keccak256("LSD_STAKING_NODE_CREATOR_ROLE");
 
-    // function testRoleChangeYieldNestOracle() public {
-    //     address newOracleAdmin = address(0xDEF);
-    //     vm.prank(actors.ORACLE_ADMIN);
-    //     YieldNestOracle.updateAdmin(newOracleAdmin);
-        
-    //     address currentOracleAdmin = YieldNestOracle.admin();
-    //     assertEq(currentOracleAdmin, newOracleAdmin);
-    // }
+        assertTrue(ynlsd.hasRole(ynlsd.DEFAULT_ADMIN_ROLE(), actors.ADMIN));
+        assertTrue(ynlsd.hasRole(STAKING_ADMIN_ROLE, actors.STAKING_ADMIN));
+        assertTrue(ynlsd.hasRole(LSD_RESTAKING_MANAGER_ROLE, actors.LSD_RESTAKING_MANAGER));
+        assertTrue(ynlsd.hasRole(LSD_STAKING_NODE_CREATOR_ROLE, actors.STAKING_NODE_CREATOR));
 
-    // function testRoleChangeStakingNodesManagerToV2() public {
-    //     address newV2Operator = address(0x101112);
-    //     vm.prank(actors.OPERATOR);
-    //     TestStakingNodesManagerV2.updateOperator(newV2Operator);
+        vm.startPrank(actors.ADMIN);
+        ynlsd.grantRole(STAKING_ADMIN_ROLE, newStakingAdmin);
+        ynlsd.revokeRole(STAKING_ADMIN_ROLE, actors.STAKING_ADMIN);
+        ynlsd.grantRole(LSD_RESTAKING_MANAGER_ROLE, newStakingAdmin);
+        ynlsd.revokeRole(LSD_RESTAKING_MANAGER_ROLE, actors.LSD_RESTAKING_MANAGER);
+        ynlsd.grantRole(LSD_STAKING_NODE_CREATOR_ROLE, newStakingAdmin);
+        ynlsd.revokeRole(LSD_STAKING_NODE_CREATOR_ROLE, actors.STAKING_NODE_CREATOR);
+        vm.stopPrank();
 
-    //     address currentV2Operator = TestStakingNodesManagerV2.operator();
-    //     assertEq(currentV2Operator, newV2Operator);
-    // }
+        assertTrue(ynlsd.hasRole(STAKING_ADMIN_ROLE, newStakingAdmin));
+        assertTrue(ynlsd.hasRole(LSD_RESTAKING_MANAGER_ROLE, newStakingAdmin));
+        assertTrue(ynlsd.hasRole(LSD_STAKING_NODE_CREATOR_ROLE, newStakingAdmin));
+        assertFalse(ynlsd.hasRole(STAKING_ADMIN_ROLE, actors.STAKING_ADMIN));
+        assertFalse(ynlsd.hasRole(LSD_RESTAKING_MANAGER_ROLE, actors.LSD_RESTAKING_MANAGER));
+        assertFalse(ynlsd.hasRole(LSD_STAKING_NODE_CREATOR_ROLE, actors.STAKING_NODE_CREATOR));
+    }
+
+    function testRoleChangeYieldNestOracle() public {
+        address newOracleAdmin = address(0xDEF);
+        bytes32 ORACLE_MANAGER_ROLE = keccak256("ORACLE_MANAGER_ROLE");
+
+        assertTrue(yieldNestOracle.hasRole(yieldNestOracle.DEFAULT_ADMIN_ROLE(), actors.ADMIN));
+        assertTrue(yieldNestOracle.hasRole(ORACLE_MANAGER_ROLE, actors.ORACLE_MANAGER));
+
+        vm.startPrank(actors.ADMIN);
+        yieldNestOracle.grantRole(ORACLE_MANAGER_ROLE, newOracleAdmin);
+        yieldNestOracle.revokeRole(ORACLE_MANAGER_ROLE, actors.ORACLE_MANAGER);
+        vm.stopPrank();
+
+        assertTrue(yieldNestOracle.hasRole(ORACLE_MANAGER_ROLE, newOracleAdmin));
+        assertFalse(yieldNestOracle.hasRole(ORACLE_MANAGER_ROLE, actors.ORACLE_MANAGER));
+    }
 }
 
 
