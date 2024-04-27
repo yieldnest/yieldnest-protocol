2402fc349d0252aeda2c8c9ace0e554ac2b92aff
danoctavian <danoctavian91@gmail.com>
2024-04-27 05:09:33 +0300

    use utility function

--- script/DeployPooledDepositsVaults.s.sol
+++ script/DeployPooledDepositsVaults.s.sol
@@ -41,27 +41,12 @@ contract Upgrade is BaseScript {
     function savePooledDepositsDeployment(PooledDepositsVault[] memory pooledDepositsVaults) internal { // Renamed from PooledDeposits to PooledDepositsVault
 
         string memory json = "deployment";
-        ActorAddresses.Actors memory actors = getActors();
-
-        // actors
-        vm.serializeAddress(json, "PROXY_ADMIN_OWNER", address(actors.admin.PROXY_ADMIN_OWNER));
-        vm.serializeAddress(json, "ADMIN", address(actors.admin.ADMIN));
-        vm.serializeAddress(json, "STAKING_ADMIN", address(actors.admin.STAKING_ADMIN));
-        vm.serializeAddress(json, "STAKING_NODES_OPERATOR", address(actors.ops.STAKING_NODES_OPERATOR)); // Assuming STAKING_NODES_ADMIN is a typo and should be STAKING_NODES_OPERATOR or another existing role in the context provided
-        vm.serializeAddress(json, "VALIDATOR_MANAGER", address(actors.ops.VALIDATOR_MANAGER));
-        vm.serializeAddress(json, "FEE_RECEIVER", address(actors.admin.FEE_RECEIVER));
-        vm.serializeAddress(json, "PAUSE_ADMIN", address(actors.admin.PAUSE_ADMIN));
-        vm.serializeAddress(json, "LSD_RESTAKING_MANAGER", address(actors.ops.LSD_RESTAKING_MANAGER));
-        vm.serializeAddress(json, "STAKING_NODE_CREATOR", address(actors.ops.STAKING_NODE_CREATOR));
-        vm.serializeAddress(json, "ORACLE_ADMIN", address(actors.admin.ORACLE_ADMIN));
-        vm.serializeAddress(json, "DEPOSIT_BOOTSTRAPPER", address(actors.eoa.DEPOSIT_BOOTSTRAPPER));
-        vm.serializeAddress(json, "POOLED_DEPOSITS_OWNER", address(actors.ops.POOLED_DEPOSITS_OWNER));
+        serializeActors(json);
 
         for (uint i = 0; i < pooledDepositsVaults.length; i++) {
             vm.serializeAddress(json, string.concat("Deposit-", vm.toString(i)), address(pooledDepositsVaults[i]));
         }
-        string memory finalJson = vm.serializeAddress(json, "DEFAULT_SIGNER", address(actors.eoa.DEFAULT_SIGNER));
-        
+        string memory finalJson = vm.serializeAddress(json, "ZEND", address(0));
 
         vm.writeJson(finalJson, getVaultsDeploymentFile());
     }
