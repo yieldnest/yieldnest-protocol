597dc7054b3238165468cda640f61c89e903ed9a
danoctavian <danoctavian91@gmail.com>
2024-05-18 04:25:12 +0300

    add test for referrals adding

--- test/integration/ReferralDepositAdapter.t.sol
+++ test/integration/ReferralDepositAdapter.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BSD 3-Clause License
 pragma solidity ^0.8.24;
 
+import { ReferralDepositAdapter } from "src/ReferralDepositAdapter.sol";
+import "forge-std/Vm.sol";
 import "test/integration/IntegrationBaseTest.sol";
 
 contract ReferralDepositAdapterTest is IntegrationBaseTest {
@@ -37,5 +39,61 @@ contract ReferralDepositAdapterTest is IntegrationBaseTest {
         vm.expectRevert(ynETH.Paused.selector);
         address referrer = vm.addr(9000);
         referralDepositAdapter.depositWithReferral{value: depositAmount}(address(this), referrer);
+    }
+
+    function testDepositWithReferralZeroAmount() public {
+        address depositor = vm.addr(3000);
+        address referrer = vm.addr(9000);
+        uint256 depositAmount = 0 ether; // Zero deposit amount
+        vm.deal(depositor, depositAmount);
+
+        vm.prank(depositor);
+        vm.expectRevert(ReferralDepositAdapter.ZeroETH.selector);
+        referralDepositAdapter.depositWithReferral{value: depositAmount}(depositor, referrer);
+    }
+
+    function testDepositWithReferralZeroAddressReferrer() public {
+        address depositor = vm.addr(3000);
+        uint256 depositAmount = 1 ether;
+        vm.deal(depositor, depositAmount);
+
+        vm.prank(depositor);
+        vm.expectRevert(ReferralDepositAdapter.ZeroAddress.selector);
+        referralDepositAdapter.depositWithReferral{value: depositAmount}(depositor, address(0));
+    }
+
+    function testDepositWithReferralZeroAddressReceiver() public {
+        address referrer = vm.addr(9000);
+        uint256 depositAmount = 1 ether;
+        vm.deal(address(0), depositAmount); // This should not be possible, but testing for code robustness
+
+        vm.expectRevert(ReferralDepositAdapter.ZeroAddress.selector);
+        referralDepositAdapter.depositWithReferral{value: depositAmount}(address(0), referrer);
+    }
+
+    function testPublishReferrals() public {
+ 
+        IReferralDepositAdapter.ReferralInfo[] memory referrals = new IReferralDepositAdapter.ReferralInfo[](1);
+        {
+                    // Arrange
+            address depositor = vm.addr(1000);
+            address referrer = vm.addr(2000);
+            address referee = vm.addr(3000);
+            uint256 amountDeposited = 1 ether;
+            uint256 shares = 100;
+            uint256 timestamp = block.timestamp;
+            referrals[0] = IReferralDepositAdapter.ReferralInfo({
+                depositor: depositor,
+                referrer: referrer,
+                referee: referee,
+                amountDeposited: amountDeposited,
+                shares: shares,
+                timestamp: timestamp
+            });
+        }
+
+        
+        vm.startPrank(actors.ops.REFERAL_PUBLISHER);
+        referralDepositAdapter.publishReferrals(referrals);
     }
 }
