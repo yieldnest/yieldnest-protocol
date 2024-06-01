6ba58c982feba1cba707e1d7ceb81178dbd217ed
danoctavian <danoctavian91@gmail.com>
2024-06-01 23:45:28 +0300

    fix compile

--- src/WithdrawalQueueManager.sol
+++ src/WithdrawalQueueManager.sol
@@ -1,7 +1,12 @@
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// SPDX-License-Identifier: BSD 3-Clause License
+pragma solidity ^0.8.24;
+
+
+import "lib/openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC721/ERC721Upgradeable.sol";
+import {AccessControlUpgradeable} from "lib/openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
+import {ReentrancyGuardUpgradeable} from "lib/openzeppelin-contracts-upgradeable/contracts/utils/ReentrancyGuardUpgradeable.sol";
 
 interface IRedemptionAdapter {
     function getRedemptionRate() external view returns (uint256);
@@ -9,7 +14,7 @@ interface IRedemptionAdapter {
     function transferRedemptionAsset(address to, uint256 amount) external;
 }
 
-contract WithdrawalQueueManager is ERC721, ReentrancyGuard, Ownable {
+contract WithdrawalQueueManager is ERC721Upgradeable, AccessControlUpgradeable, ReentrancyGuardUpgradeable {
     IERC20 public redeemableAsset;
     IERC20 public redemptionAsset;
     IRedemptionAdapter public redemptionAdapter;
@@ -26,16 +31,23 @@ contract WithdrawalQueueManager is ERC721, ReentrancyGuard, Ownable {
     event WithdrawalRequested(uint256 indexed tokenId, address requester, uint256 amount);
     event WithdrawalClaimed(uint256 indexed tokenId, address claimer, uint256 redeemedAmount);
 
-    constructor(
-        string memory name,
-        string memory symbol,
-        address _redeemableAsset,
-        address _redemptionAsset,
-        address _redemptionAdapter
-    ) ERC721(name, symbol) {
-        redeemableAsset = IERC20(_redeemableAsset);
-        redemptionAsset = IERC20(_redemptionAsset);
-        redemptionAdapter = IRedemptionAdapter(_redemptionAdapter);
+    constructor() {
+         _disableInitializers();
+    }
+
+    struct InitializationParams {
+        string name;
+        string symbol;
+        address redeemableAsset;
+        address redemptionAsset;
+        address redemptionAdapter;
+    }
+
+    function initialize(InitializationParams memory params) public initializer {
+        __ERC721_init(params.name, params.symbol);
+        redeemableAsset = IERC20(params.redeemableAsset);
+        redemptionAsset = IERC20(params.redemptionAsset);
+        redemptionAdapter = IRedemptionAdapter(params.redemptionAdapter);
     }
 
     function requestWithdrawal(uint256 amount) external nonReentrant {
@@ -49,14 +61,14 @@ contract WithdrawalQueueManager is ERC721, ReentrancyGuard, Ownable {
             redemptionRateAtRequestTime: currentRate
         });
 
-        redemptionAdapter.transferRedeemableAsset(address(this), request.amount);
+        redemptionAdapter.transferRedeemableAsset(address(this), amount);
 
         _mint(msg.sender, tokenId);
         emit WithdrawalRequested(tokenId, msg.sender, amount);
     }
 
     function claimWithdrawal(uint256 tokenId) external nonReentrant {
-        require(_isApprovedOrOwner(msg.sender, tokenId), "WithdrawalQueueManager: caller is not owner nor approved");
+        require(_ownerOf(tokenId) == msg.sender || _getApproved(tokenId) == msg.sender, "WithdrawalQueueManager: caller is not owner nor approved");
 
         WithdrawalRequest storage request = withdrawalRequests[tokenId];
         uint256 redeemAmount = (request.amount * request.redemptionRateAtRequestTime) / 1e18; // Assuming rate is scaled by 1e18
@@ -69,8 +81,9 @@ contract WithdrawalQueueManager is ERC721, ReentrancyGuard, Ownable {
         emit WithdrawalClaimed(tokenId, msg.sender, redeemAmount);
     }
 
-    function setRedemptionAdapter(address newAdapter) external onlyOwner {
-        redemptionAdapter = IRedemptionAdapter(newAdapter);
+    function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControlUpgradeable, ERC721Upgradeable) returns (bool) {
+        return interfaceId == type(IERC721).interfaceId || super.supportsInterface(interfaceId);
     }
+
 }
 
