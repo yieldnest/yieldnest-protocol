d1f48e561c03e1b6eb6f8c6f7e582a3697c2a8e3
danoctavian <danoctavian91@gmail.com>
2024-06-09 07:26:09 +0300

    refactor struct to interface

--- src/interfaces/IWithdrawalQueueManager.sol
+++ src/interfaces/IWithdrawalQueueManager.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.8.24;
 
 
 interface IWithdrawalQueueManager {
+
+    struct WithdrawalRequest {
+        uint256 amount;
+        uint256 redemptionRateAtRequestTime;
+        uint256 creationTimestamp;
+        uint256 creationBlock;
+        bool processed;
+    }
+
     function requestWithdrawal(uint256 amount) external;
     function claimWithdrawal(uint256 tokenId) external;
     function setSecondsToFinalization(uint256 _secondsToFinalization) external;
