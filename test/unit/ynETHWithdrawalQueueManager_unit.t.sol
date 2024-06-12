60c567fc1f25452ba6b58e6cfa9638d6b70c5dd7
danoctavian <danoctavian91@gmail.com>
2024-06-12 08:02:18 +0300

    add unit test for ynETHWithdrawalQueueManager

---
+++ test/unit/ynETHWithdrawalQueueManager_unit.t.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+import {ynETHWithdrawalQueueManager} from "src/ynETHWithdrawalQueueManager.sol";
+import {IRedeemableAsset} from "src/interfaces/IRedeemableAsset.sol";
+import {IWithdrawalQueueManager} from "src/interfaces/IWithdrawalQueueManager.sol";
+import {WithdrawalQueueManager} from "src/WithdrawalQueueManager.sol";
+
+import "test/unit/mocks/MockRedeemableYnETH.sol";
+import {TransparentUpgradeableProxy} from "lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract ynETHWithdrawalQueueManagerTest is Test {
+    ynETHWithdrawalQueueManager manager;
+    MockRedeemableYnETH redeemableAsset;
+    address admin = address(0x65432);
+    address withdrawalQueueAdmin = address(0x76543);
+    address user = address(0x123456);
+
+    function setUp() public {
+        redeemableAsset = new MockRedeemableYnETH();
+
+
+        ynETHWithdrawalQueueManager.Init memory init = WithdrawalQueueManager.Init({
+            name: "ynETH Withdrawal",
+            symbol: "ynETHW",
+            redeemableAsset: address(redeemableAsset),
+            admin: admin,
+            withdrawalQueueAdmin: withdrawalQueueAdmin,
+            withdrawalFee: 100, // 1%
+            feeReceiver: address(0xabc)
+        });
+
+        bytes memory initData = abi.encodeWithSelector(WithdrawalQueueManager.initialize.selector, init);
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            address(new ynETHWithdrawalQueueManager()),
+            admin, // admin of the proxy
+            initData
+        );
+        manager = ynETHWithdrawalQueueManager(address(proxy));
+
+        vm.prank(withdrawalQueueAdmin);
+        manager.setSecondsToFinalization(3 * 24 * 3600); // 3 days to finalize
+
+        uint256 initialMintAmount = 10000 ether;
+        redeemableAsset.mint(user, initialMintAmount);
+    }
+
+    function testRequestWithdrawal() public {
+        uint256 amount = 1 ether;
+        vm.prank(user);
+        redeemableAsset.approve(address(manager), amount);
+        vm.prank(user);
+        manager.requestWithdrawal(amount);
+        IWithdrawalQueueManager.WithdrawalRequest memory withdrawalRequest = manager.withdrawalRequest(0);
+        assertEq(withdrawalRequest.amount, amount, "Stored amount should match requested amount");
+    }
+
+    // function testClaimWithdrawal() public {
+    //     uint256 amount = 1 ether;
+    //     vm.prank(user);
+    //     manager.requestWithdrawal(amount);
+    //     uint256 tokenId = 1;
+
+    //     // Fast forward time to pass the finalization period
+    //     vm.warp(block.timestamp + manager.secondsToFinalization() + 1);
+
+    //     vm.prank(user);
+    //     manager.claimWithdrawal(tokenId);
+    //     bool processed = manager.withdrawalRequests(tokenId).processed;
+    //     assertTrue(processed, "Withdrawal should be marked as processed");
+    // }
+
+    // function testFailClaimWithdrawalNotFinalized() public {
+    //     uint256 amount = 1 ether;
+    //     vm.prank(user);
+    //     manager.requestWithdrawal(amount);
+    //     uint256 tokenId = 1;
+
+    //     // Attempt to claim before time is up
+    //     vm.prank(user);
+    //     manager.claimWithdrawal(tokenId);
+    // }
+
+    // function testSetSecondsToFinalization() public {
+    //     uint256 newTime = 1000;
+    //     vm.prank(withdrawalQueueAdmin);
+    //     manager.setSecondsToFinalization(newTime);
+    //     assertEq(manager.secondsToFinalization(), newTime, "Seconds to finalization should be updated");
+    // }
+
+    // function testSetWithdrawalFee() public {
+    //     uint256 newFee = 200; // 2%
+    //     vm.prank(withdrawalQueueAdmin);
+    //     manager.setWithdrawalFee(newFee);
+    //     assertEq(manager.withdrawalFee(), newFee, "Withdrawal fee should be updated");
+    // }
+}
