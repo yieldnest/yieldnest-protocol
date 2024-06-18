d4d8f3c5345093a64829cdad60b361701a4b1c01
danoctavian <danoctavian91@gmail.com>
2024-06-18 14:39:29 +0300

    separate logic of sending redemption assets into a vault

--- test/unit/ynETHWithdrawalQueueManager_unit.t.sol
+++ test/unit/ynETHWithdrawalQueueManager_unit.t.sol
@@ -3,30 +3,43 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
 import {Math} from "lib/openzeppelin-contracts/contracts/utils/math/Math.sol";
-import {ynETHWithdrawalQueueManager} from "src/ynETHWithdrawalQueueManager.sol";
+import {WithdrawalQueueManager} from "src/WithdrawalQueueManager.sol";
 import {IRedeemableAsset} from "src/interfaces/IRedeemableAsset.sol";
 import {IWithdrawalQueueManager} from "src/interfaces/IWithdrawalQueueManager.sol";
 import {WithdrawalQueueManager} from "src/WithdrawalQueueManager.sol";
+import {MockRedeemableYnETH} from "test/unit/mocks/MockRedeemableYnETH.sol";
+import { ynETHRedemptionAssetsVault } from "src/ynETHRedemptionAssetsVault.sol";
+import { IRedemptionAssetsVault } from "src/interfaces/IRedemptionAssetsVault.sol";
+import { IynETH } from "src/interfaces/IynETH.sol";
 
-import "test/unit/mocks/MockRedeemableYnETH.sol";
 import {TransparentUpgradeableProxy} from "lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 
 contract ynETHWithdrawalQueueManagerTest is Test {
-    ynETHWithdrawalQueueManager manager;
+    WithdrawalQueueManager manager;
     MockRedeemableYnETH redeemableAsset;
     address admin = address(0x65432);
     address withdrawalQueueAdmin = address(0x76543);
     address user = address(0x123456);
     address feeReceiver = address(0xabc);
+    ynETHRedemptionAssetsVault redemptionAssetsVault;
 
     function setUp() public {
         redeemableAsset = new MockRedeemableYnETH();
 
-        ynETHWithdrawalQueueManager.Init memory init = WithdrawalQueueManager.Init({
+        ynETHRedemptionAssetsVault redemptionAssetsVaultImplementation = new ynETHRedemptionAssetsVault();
+        TransparentUpgradeableProxy redemptionAssetsVaultProxy = new TransparentUpgradeableProxy(
+            address(redemptionAssetsVaultImplementation),
+            admin, // admin of the proxy
+            ""
+        );
+        redemptionAssetsVault = ynETHRedemptionAssetsVault(payable(address(redemptionAssetsVaultProxy)));
+
+        WithdrawalQueueManager.Init memory init = WithdrawalQueueManager.Init({
             name: "ynETH Withdrawal",
             symbol: "ynETHW",
-            redeemableAsset: address(redeemableAsset),
+            redeemableAsset: redeemableAsset,
+            redemptionAssetsVault: IRedemptionAssetsVault((address(redemptionAssetsVault))),
             admin: admin,
             withdrawalQueueAdmin: withdrawalQueueAdmin,
             withdrawalFee: 10000, // 1%
@@ -35,12 +48,20 @@ contract ynETHWithdrawalQueueManagerTest is Test {
 
         bytes memory initData = abi.encodeWithSelector(WithdrawalQueueManager.initialize.selector, init);
         TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
-            address(new ynETHWithdrawalQueueManager()),
+            address(new WithdrawalQueueManager()),
             admin, // admin of the proxy
             initData
         );
 
-        manager = ynETHWithdrawalQueueManager(payable(address(proxy)));
+        manager = WithdrawalQueueManager(payable(address(proxy)));
+
+        ynETHRedemptionAssetsVault.Init memory vaultInit = ynETHRedemptionAssetsVault.Init({
+            admin: admin,
+            redeemer: address(manager),
+            ynETH: IynETH(address(redeemableAsset))
+        });
+        redemptionAssetsVault.initialize(vaultInit);
+
 
         vm.prank(withdrawalQueueAdmin);
         manager.setSecondsToFinalization(3 * 24 * 3600); // 3 days to finalize
@@ -74,7 +95,7 @@ contract ynETHWithdrawalQueueManagerTest is Test {
         assertEq(withdrawalRequest.amount, amount, "Stored amount should match requested amount");
 
         assertEq(withdrawalRequest.feeAtRequestTime, manager.withdrawalFee(), "Stored fee should match current withdrawal fee");
-        assertEq(withdrawalRequest.redemptionRateAtRequestTime, manager.redemptionRate(), "Stored redemption rate should match current redemption rate");
+        assertEq(withdrawalRequest.redemptionRateAtRequestTime, redemptionAssetsVault.redemptionRate(), "Stored redemption rate should match current redemption rate");
         assertEq(withdrawalRequest.creationTimestamp, block.timestamp, "Stored creation timestamp should match current block timestamp");
         assertEq(withdrawalRequest.creationBlock, block.number, "Stored creation block should match current block number");
         assertEq(withdrawalRequest.processed, false, "Stored processed status should be false");
@@ -90,14 +111,14 @@ contract ynETHWithdrawalQueueManagerTest is Test {
         manager.requestWithdrawal(amount);
         uint256 creationBlock = block.number;
         uint256 tokenId = 0;
-        uint256 redemptionRateAtRequestTime = manager.redemptionRate();
+        uint256 redemptionRateAtRequestTime = redemptionAssetsVault.redemptionRate();
 
 
         // Fast forward time to pass the finalization period
         vm.warp(block.timestamp + manager.secondsToFinalization() + 1);
 
-        // Send exact Ether to manager
-        (bool success, ) = address(manager).call{value: amount}("");
+        // Send exact Ether to vault
+        (bool success, ) = address(redemptionAssetsVault).call{value: amount}("");
         require(success, "Ether transfer failed");
 
         vm.prank(user);
