02b30b4bce7c1a91dfd3933e7f7171af80f7c753
danoctavian <danoctavian91@gmail.com>
2024-06-22 07:43:44 +0300

    add Constants file

---
+++ src/Constants.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: BSD 3-Clause License
+pragma solidity ^0.8.24;
+
+address constant ETH_ASSET = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+uint256 constant YNETH_UNIT = 1e18;
