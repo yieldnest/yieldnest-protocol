216eef7e6babcaf745b59f4a8516fc535a175b47
johnnyonline <johnnyonline.eth@gmail.com>
2024-06-28 12:39:48 +0300

    fix: https://github.com/yieldnest/yieldnest-protocol/pull/119#discussion_r1658162528

--- src/ynViewer.sol
+++ src/ynViewer.sol
@@ -3,20 +3,24 @@ pragma solidity ^0.8.24;
 
 import {IDelegationManager, IStrategy} from "@eigenlayer/src/contracts/interfaces/IDelegationManager.sol";
 import {IEigenPod} from "@eigenlayer/src/contracts/interfaces/IEigenPod.sol";
-import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 import {IStakingNode, IStakingNodesManager, IynViewer} from "./interfaces/IynViewer.sol";
 
+interface IynETH {
+    function totalSupply() external view returns (uint256);
+    function totalAssets() external view returns (uint256);
+}
+
 contract ynViewer is IynViewer {
 
-    IERC4626 public immutable ynETH;
+    IynETH public immutable ynETH;
     IStakingNodesManager public immutable stakingNodesManager;
 
     /// @notice Initializes a new ynViewer contract.
     /// @param _ynETH The address of the ynETH contract.
     /// @param _stakingNodesManager The address of the StakingNodesManager contract.
     constructor(address _ynETH, address _stakingNodesManager) {
-        ynETH = IERC4626(_ynETH);
+        ynETH = IynETH(_ynETH);
         stakingNodesManager = IStakingNodesManager(_stakingNodesManager);
     }
 
