942992303239edf575546c7f37b66b88ac9433d3
danoctavian <danoctavian91@gmail.com>
2024-06-30 02:29:58 +0300

    refactor to StakingNodeTestBase

---
+++ test/utils/StakingNodeTestBase.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: BSD 3-Clause License
+pragma solidity ^0.8.24;
+import {StakingNodesManager} from "src/StakingNodesManager.sol";
+import {OwnableUpgradeable} from "lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import {ynETH} from "src/ynETH.sol";
+import {RewardsReceiver} from "src/RewardsReceiver.sol";
+import {RewardsDistributor} from "src/RewardsDistributor.sol";
+import {ProxyAdmin} from "lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
+import {IRewardsDistributor} from "src/interfaces/IRewardsDistributor.sol";
+import {IStakingNodesManager} from "src/interfaces/IStakingNodesManager.sol";
+import {IStakingNode} from "src/interfaces/IStakingNodesManager.sol";
+import {IBeaconChainOracle} from "lib/eigenlayer-contracts/src/contracts/interfaces/IBeaconChainOracle.sol";
+import {IStrategy} from "lib/eigenlayer-contracts/src/contracts/interfaces/IStrategy.sol";
+import {IDelegationManager} from "lib/eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
+import {TransparentUpgradeableProxy} from "lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {ITransparentUpgradeableProxy} from "lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {ScenarioBaseTest} from "test/scenarios/ScenarioBaseTest.sol";
+import { Invariants } from "test/scenarios/Invariants.sol";
+import {stdStorage, StdStorage} from "forge-std/Test.sol"; 
+import {BytesLib} from "lib/eigenlayer-contracts/src/contracts/libraries/BytesLib.sol";
+import { MockEigenLayerBeaconOracle } from "test/mocks/MockEigenLayerBeaconOracle.sol";
+
+import {UpgradeableBeacon} from "lib/openzeppelin-contracts/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {TestStakingNodesManagerV2} from "test/mocks/TestStakingNodesManagerV2.sol";
+import {TestStakingNodeV2} from "test/mocks/TestStakingNodeV2.sol";
+
+import {BeaconChainProofs} from "lib/eigenlayer-contracts/src/contracts/libraries/BeaconChainProofs.sol";
+import {Merkle} from "lib/eigenlayer-contracts/src/contracts/libraries/Merkle.sol";
+import { ProofParsingV1 } from "test/eigenlayer-utils/ProofParsingV1.sol";
+import {Utils} from "script/Utils.sol";
+import {beaconChainETHStrategy} from "src/Constants.sol";
+import {Vm} from "lib/forge-std/src/Vm.sol";
+
+contract StakingNodeTestBase is ScenarioBaseTest, ProofParsingV1 {
+
+    struct ValidatorProofs {
+        BeaconChainProofs.StateRootProof stateRootProof;
+        uint40[] validatorIndices;
+        bytes[] withdrawalCredentialProofs;
+        bytes[] validatorFieldsProofs;
+        bytes32[][] validatorFields;
+    }
+
+    struct ValidatorWithdrawalProofParams {
+        BeaconChainProofs.StateRootProof stateRootProof;
+        bytes32[][] validatorFields;
+        bytes[] validatorFieldsProofs;
+        bytes32[][] withdrawalFields;
+        BeaconChainProofs.WithdrawalProof[] withdrawalProofs;
+    }
+
+    function _getLatestBlockRoot() public returns (bytes32) {
+        return getLatestBlockRoot();
+    }
+
+    function getWithdrawalCredentialParams() public returns (ValidatorProofs memory) {
+        ValidatorProofs memory validatorProofs;
+        
+        validatorProofs.validatorIndices = new uint40[](1);
+        validatorProofs.withdrawalCredentialProofs = new bytes[](1);
+        validatorProofs.validatorFieldsProofs = new bytes[](1);
+        validatorProofs.validatorFields = new bytes32[][](1);
+
+
+        //bytes memory validatorFieldsProof = abi.encodePacked(getValidatorProof());
+        // Set beacon state root, validatorIndex
+        validatorProofs.stateRootProof.beaconStateRoot = getBeaconStateRoot();
+        validatorProofs.stateRootProof.proof = getStateRootProof();
+        validatorProofs.validatorIndices[0] = uint40(getValidatorIndex());
+        validatorProofs.withdrawalCredentialProofs[0] = abi.encodePacked(getWithdrawalCredentialProof()); // Validator fields are proven here
+        //validatorProofs.validatorFieldsProofs[0] = validatorFieldsProof;
+        validatorProofs.validatorFields[0] = getValidatorFields();
+
+        return validatorProofs;
+    }
+
+    function getValidatorWithdrawalProofParams() public returns (ValidatorWithdrawalProofParams memory) {
+        ValidatorWithdrawalProofParams memory params;
+
+        params.validatorFieldsProofs = new bytes[](1);
+        params.validatorFields = new bytes32[][](1);
+        params.withdrawalFields = new bytes32[][](1);
+        params.withdrawalProofs =  new BeaconChainProofs.WithdrawalProof[](1);
+
+        params.stateRootProof.beaconStateRoot = getBeaconStateRoot();
+        params.stateRootProof.proof = getStateRootProof();
+        params.validatorFields[0] = getValidatorFields();
+        params.withdrawalFields[0] = getWithdrawalFields();
+        params.withdrawalProofs[0] = _getWithdrawalProof();
+        params.validatorFieldsProofs[0] = abi.encodePacked(getValidatorProof());
+
+        return params;
+    }
+
+    function bytes32ToData(bytes32 data) public pure returns (address) {
+        return address(uint160(uint256(data)));
+    }
+
+    function getWithdrawalAddress() public returns (address) {
+        bytes32[] memory validatorFields = getValidatorFields();
+        return bytes32ToData(validatorFields[1]);
+    }
+
+
+    function _getWithdrawalProof() internal returns (BeaconChainProofs.WithdrawalProof memory) {
+        {
+            bytes32 blockRoot = getBlockRoot();
+            bytes32 slotRoot = getSlotRoot();
+            bytes32 timestampRoot = getTimestampRoot();
+            bytes32 executionPayloadRoot = getExecutionPayloadRoot();
+
+            return
+                BeaconChainProofs.WithdrawalProof(
+                    abi.encodePacked(getWithdrawalProofDeneb()),
+                    abi.encodePacked(getSlotProof()),
+                    abi.encodePacked(getExecutionPayloadProof()),
+                    abi.encodePacked(getTimestampProofDeneb()),
+                    abi.encodePacked(getHistoricalSummaryProof()),
+                    uint64(getBlockRootIndex()),
+                    uint64(getHistoricalSummaryIndex()),
+                    uint64(getWithdrawalIndex()),
+                    blockRoot,
+                    slotRoot,
+                    timestampRoot,
+                    executionPayloadRoot
+                );
+        }
+    }
+}
