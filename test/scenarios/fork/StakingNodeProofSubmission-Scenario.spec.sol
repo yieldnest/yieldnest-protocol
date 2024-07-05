19e47930524d405e6c8446316932f545605c336d
danoctavian <danoctavian91@gmail.com>
2024-07-05 15:01:07 +0300

    move invariant function to StakingNodeBase test

--- test/scenarios/fork/StakingNodeProofSubmission-Scenario.spec.sol
+++ test/scenarios/fork/StakingNodeProofSubmission-Scenario.spec.sol
@@ -535,24 +535,4 @@ contract StakingNodeVerifyWithdrawalCredentialsOnHolesky is StakingNodeTestBase 
         }
         return balances;
     }
-
-    function runSystemStateInvariants(
-        uint256 previousTotalAssets,
-        uint256 previousTotalSupply,
-        uint256[] memory previousStakingNodeBalances
-    ) public {  
-        assertEq(yneth.totalAssets(), previousTotalAssets, "Total assets integrity check failed");
-        assertEq(yneth.totalSupply(), previousTotalSupply, "Share mint integrity check failed");
-        for (uint i = 0; i < previousStakingNodeBalances.length; i++) {
-            IStakingNode stakingNodeInstance = stakingNodesManager.nodes(i);
-            uint256 currentStakingNodeBalance = stakingNodeInstance.getETHBalance();
-            assertEq(currentStakingNodeBalance, previousStakingNodeBalances[i], "Staking node balance integrity check failed for node ID: ");
-        }
-	}
-
-    function runStakingNodeInvariants(uint256 nodeId) public {
-        IStakingNode stakingNodeInstance = stakingNodesManager.nodes(nodeId);
-        uint256 unverifiedStakedETH = stakingNodeInstance.getUnverifiedStakedETH();
-        int256 shares = eigenPodManager.podOwnerShares(address(stakingNodeInstance));
-    }
 }
