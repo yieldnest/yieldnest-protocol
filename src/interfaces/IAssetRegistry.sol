01e19e95dac903ab768899ae2c52c721c764040f
danoctavian <danoctavian91@gmail.com>
2024-07-07 12:19:39 +0300

    add assetIsSupported correct impl

--- src/interfaces/IAssetRegistry.sol
+++ src/interfaces/IAssetRegistry.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BSD 3-Clause License
 pragma solidity ^0.8.24;
 
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 interface IAssetRegistry {
     function addAsset(address asset, uint256 initialBalance) external;
     function disableAsset(address asset) external;
@@ -8,4 +10,5 @@ interface IAssetRegistry {
     function assetIsSupported(address asset) external view returns (bool);
     function totalAssets() external view returns (uint256);
     function convertToUnitOfAccount(address asset, uint256 amount) external view returns (uint256);
+    function assetIsSupported(IERC20 asset) public returns (bool);
 }
