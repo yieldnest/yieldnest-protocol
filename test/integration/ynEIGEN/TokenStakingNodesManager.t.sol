b166cc26f1dce50b6f806ccc024e76ebb44b88d9
danoctavian <danoctavian91@gmail.com>
2024-07-09 14:06:52 +0300

    add asset retrieval tests wip

--- test/integration/ynEIGEN/TokenStakingNodesManager.t.sol
+++ test/integration/ynEIGEN/TokenStakingNodesManager.t.sol
@@ -106,59 +106,6 @@ contract TokenStakingNodesManagerAdminTest is ynEigenIntegrationBaseTest {
         // TODO: Come back to this
     }
 
-    // function testRetrieveAssetsNotLSDStakingNode() public {
-    //     IERC20 asset = IERC20(chainAddresses.lsd.RETH_ADDRESS);
-    //     uint256 amount = 1000;
-
-    //     vm.prank(actors.ops.STAKING_NODE_CREATOR);
-    //     ynlsd.createLSDStakingNode();
-    //     vm.expectRevert(abi.encodeWithSelector(ynLSD.NotLSDStakingNode.selector, address(this), 0));
-    //     ynlsd.retrieveAsset(0, asset, amount);
-    // }
-
-    // function testRetrieveAssetsUnsupportedAsset() public {
-    //     // come back to this
-    // }
-
-    // function testRetrieveTransferExceedsBalance() public {
-    //     IERC20 asset = IERC20(chainAddresses.lsd.RETH_ADDRESS);
-    //     uint256 amount = 1000;
-
-    //     vm.prank(actors.ops.STAKING_NODE_CREATOR);
-    //     ynlsd.createTokenStakingNode();
-
-    //     ITokenStakingNode tokenStakingNode = ynlsd.nodes(0);
-
-    //     vm.startPrank(address(tokenStakingNode));
-    //     vm.expectRevert();
-    //     ynlsd.retrieveAsset(0, asset, amount);
-    //     vm.stopPrank();
-    // }
-
-    // function testRetrieveAssetsSuccess() public {
-    //     IERC20 asset = IERC20(chainAddresses.lsd.STETH_ADDRESS);
-    //     uint256 amount = 64 ether;
-
-    //     vm.prank(actors.ops.STAKING_NODE_CREATOR);
-    //     ynlsd.createTokenStakingNode();
-
-    //     ITokenStakingNode tokenStakingNode = ynlsd.nodes(0);
-    //     vm.deal(address(tokenStakingNode), 1000);
-
-    //     // 1. Obtain stETH and Deposit assets to ynLSD by User
-    //     TestAssetUtils testAssetUtils = new TestAssetUtils();
-    //     uint256 balance = testAssetUtils.get_stETH(address(this), amount);
-    //     assertEq(compareRebasingTokenBalances(balance, amount), true, "Amount not received");
-       
-    //     asset.approve(address(ynlsd), balance);
-    //     ynlsd.deposit(asset, balance, address(this));
-
-    //     vm.startPrank(address(tokenStakingNode));
-    //     asset.approve(address(ynlsd), 32 ether);
-    //     ynlsd.retrieveAsset(0, asset, balance);
-    //     vm.stopPrank();
-    // }
-
     function testSetMaxNodeCount() public {
         uint256 maxNodeCount = 10;
         vm.prank(actors.admin.STAKING_ADMIN);
