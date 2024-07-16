4259333e646dd822df553d59cbf50f4a0f940e90
danoctavian <danoctavian91@gmail.com>
2024-07-16 19:39:10 +0300

    use new TestAssetUtils

--- test/integration/ynEIGEN/AssetRegistry.t.sol
+++ test/integration/ynEIGEN/AssetRegistry.t.sol
@@ -20,6 +20,13 @@ import "forge-std/console.sol";
  */
 
 contract AssetRegistryTest is ynEigenIntegrationBaseTest {
+
+    TestAssetUtils testAssetUtils;
+
+    constructor() {
+        testAssetUtils = new TestAssetUtils();
+    }
+    
     function testTotalAssetsWithFuzzedDeposits(uint256 wstethAmount, uint256 woethAmount, uint256 rethAmount) public {
         vm.assume(
             wstethAmount < 100 ether && wstethAmount >= 1 wei &&
@@ -27,42 +34,21 @@ contract AssetRegistryTest is ynEigenIntegrationBaseTest {
             rethAmount < 100 ether && rethAmount >= 1 wei
         );
 
-        TestAssetUtils testAssetUtils = new TestAssetUtils();
-
         {
-            // Deposit wstETH
-            IERC20 wstETH = IERC20(chainAddresses.lsd.WSTETH_ADDRESS);
             address prankedUserWstETH = address(uint160(uint256(keccak256(abi.encodePacked("wstETHUser")))));
-            TestAssetUtils testAssetUtils = new TestAssetUtils();
-            testAssetUtils.get_wstETH(prankedUserWstETH, wstethAmount);
-
-            vm.prank(prankedUserWstETH);
-            wstETH.approve(address(ynEigenToken), wstethAmount);
-            vm.prank(prankedUserWstETH);
-            ynEigenToken.deposit(wstETH, wstethAmount, prankedUserWstETH);
+            depositAsset(chainAddresses.lsd.WSTETH_ADDRESS, wstethAmount, prankedUserWstETH);
         }
 
         {
-            // Deposit woETH
-            IERC20 woETH = IERC20(chainAddresses.lsd.WOETH_ADDRESS);
+            // Deposit woETH using utility function
             address prankedUserWoETH = address(uint160(uint256(keccak256(abi.encodePacked("woETHUser")))));
-            vm.prank(prankedUserWoETH);
-            testAssetUtils.get_wOETH(prankedUserWoETH, woethAmount);
-            vm.prank(prankedUserWoETH);
-            woETH.approve(address(ynEigenToken), woethAmount);
-            vm.prank(prankedUserWoETH);
-            ynEigenToken.deposit(woETH, woethAmount, prankedUserWoETH);
+            depositAsset(chainAddresses.lsd.WOETH_ADDRESS, woethAmount, prankedUserWoETH);
         }
 
         {
-            // Deposit rETH
-            IERC20 rETH = IERC20(chainAddresses.lsd.RETH_ADDRESS);
+            // Deposit rETH using utility function
             address prankedUserRETH = address(uint160(uint256(keccak256(abi.encodePacked("rETHUser")))));
-            testAssetUtils.get_rETH(prankedUserRETH, rethAmount);
-            vm.prank(prankedUserRETH);
-            rETH.approve(address(ynEigenToken), rethAmount);
-            vm.prank(prankedUserRETH);
-            ynEigenToken.deposit(rETH, rethAmount, prankedUserRETH);
+            depositAsset(chainAddresses.lsd.RETH_ADDRESS, rethAmount, prankedUserRETH);
         }
 
         uint256 wstethRate = rateProvider.rate(chainAddresses.lsd.WSTETH_ADDRESS);
@@ -109,5 +95,14 @@ contract AssetRegistryTest is ynEigenIntegrationBaseTest {
         vm.prank(actors.admin.UNPAUSE_ADMIN);
         assetRegistry.unpauseActions();
         assertFalse(assetRegistry.actionsPaused(), "Actions should be unpaused");
+    }
+
+    function depositAsset(address assetAddress, uint256 amount, address user) internal {
+        IERC20 asset = IERC20(assetAddress);
+        testAssetUtils.get_Asset(assetAddress, user, amount);
+        vm.prank(user);
+        asset.approve(address(ynEigenToken), amount);
+        vm.prank(user);
+        ynEigenToken.deposit(asset, amount, user);
     }
 }
