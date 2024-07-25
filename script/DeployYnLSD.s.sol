06bb69290238a89ddaef0aa36e2eb19691e2271b
danoctavian <danoctavian91@gmail.com>
2024-07-25 11:50:08 +0300

    wip

--- script/DeployYnLSD.s.sol
+++ script/DeployYnLSD.s.sol
@@ -1,129 +1,182 @@
 // SPDX-License-Identifier: BSD 3-Clause License
 pragma solidity ^0.8.24;
 
-// import {TransparentUpgradeableProxy} from "lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-// import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-// import {IEigenPodManager} from "lib/eigenlayer-contracts/src/contracts/interfaces/IEigenPodManager.sol";
-// import {IDelegationManager} from "lib/eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
-// import {IDelayedWithdrawalRouter} from "lib/eigenlayer-contracts/src/contracts/interfaces/IDelayedWithdrawalRouter.sol";
-// import {IStrategyManager} from "lib/eigenlayer-contracts/src/contracts/interfaces/IStrategyManager.sol";
-// import {IStrategy} from "lib/eigenlayer-contracts/src/contracts/interfaces/IStrategy.sol";
-// import {IDepositContract} from "src/external/ethereum/IDepositContract.sol";
-// import {IWETH} from "src/external/tokens/IWETH.sol";
+import {TransparentUpgradeableProxy} from "lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IEigenPodManager} from "lib/eigenlayer-contracts/src/contracts/interfaces/IEigenPodManager.sol";
+import {IDelegationManager} from "lib/eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
+import {IDelayedWithdrawalRouter} from "lib/eigenlayer-contracts/src/contracts/interfaces/IDelayedWithdrawalRouter.sol";
+import {IStrategyManager} from "lib/eigenlayer-contracts/src/contracts/interfaces/IStrategyManager.sol";
+import {IStrategy} from "lib/eigenlayer-contracts/src/contracts/interfaces/IStrategy.sol";
+import {IDepositContract} from "src/external/ethereum/IDepositContract.sol";
+import {IWETH} from "src/external/tokens/IWETH.sol";
 
-// import {ynLSD} from "src/ynLSD.sol";
-// import {LSDStakingNode} from "src/LSDStakingNode.sol";
-// import {ContractAddresses} from "script/ContractAddresses.sol";
-// import {ActorAddresses} from "script/Actors.sol";
-// import {BaseScript} from "script/BaseScript.s.sol";
-// import {console} from "lib/forge-std/src/console.sol";
+import {IynEigen} from "src/interfaces/IynEigen.sol";
+import {IRateProvider} from "src/interfaces/IRateProvider.sol";
+import {IAssetRegistry} from "src/interfaces/IAssetRegistry.sol";
+import {IEigenStrategyManager} from "src/interfaces/IEigenStrategyManager.sol";
+import {ynEigen} from "src/ynEIGEN/ynEigen.sol";
+import {TokenStakingNode} from "src/ynEIGEN/TokenStakingNode.sol";
+import {LSDRateProvider} from "src/ynEIGEN/LSDRateProvider.sol";
+import {EigenStrategyManager} from "src/ynEIGEN/EigenStrategyManager.sol";
+import {AssetRegistry} from "src/ynEIGEN/AssetRegistry.sol";
+import {TokenStakingNodesManager} from "src/ynEIGEN/TokenStakingNodesManager.sol";
 
-contract DeployYnLSD {
-    // ynLSD public ynlsd;
-    // YieldNestOracle public yieldNestOracle;
 
-    // IEigenPodManager public eigenPodManager;
-    // IDelegationManager public delegationManager;
-    // IDelayedWithdrawalRouter public delayedWithdrawalRouter;
-    // IStrategyManager public strategyManager;
-    // IDepositContract public depositContract;
-    // IWETH public weth;
+import {ContractAddresses} from "script/ContractAddresses.sol";
+import {ActorAddresses} from "script/Actors.sol";
+import {BaseScript} from "script/BaseScript.s.sol";
 
-    // function run() external {
-    //     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+import {console} from "lib/forge-std/src/console.sol";
 
-    //     // ynETH.sol ROLES
-    //     ActorAddresses.Actors memory actors = getActors();
+contract DeployYnLSD is BaseScript {
 
-    //     address _broadcaster = vm.addr(deployerPrivateKey);
+    IDelegationManager public delegationManager;
+    IDelayedWithdrawalRouter public delayedWithdrawalRouter;
+    IStrategyManager public strategyManager;
+    IEigenPodManager public eigenPodManager;
 
-    //     vm.startBroadcast(deployerPrivateKey);
+    ynEigen ynLSDe;
+    LSDRateProvider lsdRateProvider;
+    EigenStrategyManager eigenStrategyManager;
+    TokenStakingNodesManager tokenStakingNodesManager;
+    AssetRegistry assetRegistry;
 
-    //     // solhint-disable-next-line no-console
-    //     console.log("Default Signer Address:", _broadcaster);
-    //     // solhint-disable-next-line no-console
-    //     console.log("Current Block Number:", block.number);
-    //     // solhint-disable-next-line no-console
-    //     console.log("Current Chain ID:", block.chainid);
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
-    //     ContractAddresses contractAddresses = new ContractAddresses();
-    //     ContractAddresses.ChainAddresses memory chainAddresses = contractAddresses.getChainAddresses(block.chainid);
-    //     eigenPodManager = IEigenPodManager(chainAddresses.eigenlayer.EIGENPOD_MANAGER_ADDRESS);
-    //     delegationManager = IDelegationManager(chainAddresses.eigenlayer.DELEGATION_MANAGER_ADDRESS);
-    //     delayedWithdrawalRouter = IDelayedWithdrawalRouter(chainAddresses.eigenlayer.DELAYED_WITHDRAWAL_ROUTER_ADDRESS); // Assuming DEPOSIT_2_ADDRESS is used for DelayedWithdrawalRouter
-    //     strategyManager = IStrategyManager(chainAddresses.eigenlayer.STRATEGY_MANAGER_ADDRESS);
-    //     depositContract = IDepositContract(chainAddresses.ethereum.DEPOSIT_2_ADDRESS);
-    //     weth = IWETH(chainAddresses.ethereum.WETH_ADDRESS);
+        // ynETH.sol ROLES
+        ActorAddresses.Actors memory actors = getActors();
 
-    //     // Deploy implementations
-    //     {
-    //         ynLSD ynLSDImplementation = new ynLSD();
-    //         TransparentUpgradeableProxy ynLSDProxy = new TransparentUpgradeableProxy(address(ynLSDImplementation), actors.admin.PROXY_ADMIN_OWNER, "");
-    //         ynlsd = ynLSD(address(ynLSDProxy));
-    //     }
+        address _broadcaster = vm.addr(deployerPrivateKey);
 
-    //     {
-    //         YieldNestOracle yieldNestOracleImplementation  = new YieldNestOracle();
-    //         TransparentUpgradeableProxy yieldNestOracleProxy = new TransparentUpgradeableProxy(address(yieldNestOracleImplementation), actors.admin.PROXY_ADMIN_OWNER, "");
-    //         yieldNestOracle = YieldNestOracle(address(yieldNestOracleProxy));
-    //     }
+        vm.startBroadcast(deployerPrivateKey);
 
-    //     IERC20[] memory assets = new IERC20[](2);
-    //     assets[0] = IERC20(chainAddresses.lsd.RETH_ADDRESS);
-    //     assets[1] = IERC20(chainAddresses.lsd.STETH_ADDRESS);
+        // solhint-disable-next-line no-console
+        console.log("Default Signer Address:", _broadcaster);
+        // solhint-disable-next-line no-console
+        console.log("Current Block Number:", block.number);
+        // solhint-disable-next-line no-console
+        console.log("Current Chain ID:", block.chainid);
 
-    //     IStrategy[] memory strategies = new IStrategy[](2);
-    //     strategies[0] = IStrategy(chainAddresses.lsd.RETH_STRATEGY_ADDRESS);
-    //     strategies[1] = IStrategy(chainAddresses.lsd.STETH_STRATEGY_ADDRESS);
-    //     // Initialize ynLSD with example parameters
-    //     {
-    //         address[] memory lsdPauseWhitelist = new address[](1);
-    //         lsdPauseWhitelist[0] = _broadcaster;
+        ContractAddresses contractAddresses = new ContractAddresses();
+        ContractAddresses.ChainAddresses memory chainAddresses = contractAddresses.getChainAddresses(block.chainid);
+        eigenPodManager = IEigenPodManager(chainAddresses.eigenlayer.EIGENPOD_MANAGER_ADDRESS);
+        delegationManager = IDelegationManager(chainAddresses.eigenlayer.DELEGATION_MANAGER_ADDRESS);
+        delayedWithdrawalRouter = IDelayedWithdrawalRouter(chainAddresses.eigenlayer.DELAYED_WITHDRAWAL_ROUTER_ADDRESS); // Assuming DEPOSIT_2_ADDRESS is used for DelayedWithdrawalRouter
+        strategyManager = IStrategyManager(chainAddresses.eigenlayer.STRATEGY_MANAGER_ADDRESS);
 
-    //         ynLSD.Init memory ynlsdInit = ynLSD.Init({
-    //             assets: assets,
-    //             strategies: strategies,
-    //             strategyManager: strategyManager,
-    //             delegationManager: delegationManager,
-    //             oracle: yieldNestOracle,
-    //             maxNodeCount: 10,
-    //             admin: actors.admin.ADMIN,
-    //             pauser: actors.ops.PAUSE_ADMIN,
-    //             unpauser: actors.admin.UNPAUSE_ADMIN,
-    //             stakingAdmin: actors.admin.STAKING_ADMIN,
-    //             lsdRestakingManager: actors.ops.LSD_RESTAKING_MANAGER,
-    //             lsdStakingNodeCreatorRole: actors.ops.STAKING_NODE_CREATOR,
-    //             pauseWhitelist: lsdPauseWhitelist,
-    //             depositBootstrapper: actors.eoa.DEPOSIT_BOOTSTRAPPER
-    //         });
-    //         ynlsd.initialize(ynlsdInit);
-    //     }
+        // Deploy implementations
+        {
+            ynEigen ynLSDeImplementation = new ynEigen();
+            TransparentUpgradeableProxy ynLSDeProxy = new TransparentUpgradeableProxy(address(ynLSDeImplementation), actors.admin.PROXY_ADMIN_OWNER, "");
+            ynLSDe = ynEigen(address(ynLSDeProxy));
+        }
 
-    //     uint256[] memory maxAgesArray = new uint256[](assets.length);
-    //     address[] memory priceFeedAddresses = new address[](assets.length);
-    //     for (uint256 i = 0; i < assets.length; i++) {
-    //         maxAgesArray[i] = type(uint256).max;
-    //         if (assets[i] == IERC20(chainAddresses.lsd.RETH_ADDRESS)) {
-    //             priceFeedAddresses[i] = chainAddresses.lsd.RETH_FEED_ADDRESS;
-    //         } else if (assets[i] == IERC20(chainAddresses.lsd.STETH_ADDRESS)) {
-    //             priceFeedAddresses[i] = chainAddresses.lsd.STETH_FEED_ADDRESS;
-    //         }
-    //     }
+        {
+            LSDRateProvider lsdRateProviderImplementation = new LSDRateProvider();
+            TransparentUpgradeableProxy lsdRateProviderProxy = new TransparentUpgradeableProxy(address(lsdRateProviderImplementation), actors.admin.PROXY_ADMIN_OWNER, "");
+            lsdRateProvider = LSDRateProvider(address(lsdRateProviderProxy));
+        }
 
-    //     {
-    //         address[] memory assetsAddresses = new address[](assets.length);
-    //         for (uint256 i = 0; i < assets.length; i++) {
-    //             assetsAddresses[i] = address(assets[i]);
-    //         }
-    //         YieldNestOracle.Init memory yieldNestOracleInit = YieldNestOracle.Init({
-    //             assets: assetsAddresses,
-    //             priceFeedAddresses: priceFeedAddresses,
-    //             maxAges: maxAgesArray,
-    //             admin: actors.admin.ORACLE_ADMIN,
-    //             oracleManager: actors.admin.ORACLE_ADMIN
-    //         });
-    //         yieldNestOracle.initialize(yieldNestOracleInit);
-    //     }
+        IERC20[] memory assets;
+        IStrategy[] memory strategies;
+
+        if (block.chainid == 1) {
+
+            assets = new IERC20[](2);
+            assets[0] = IERC20(chainAddresses.lsd.WSTETH_ADDRESS);
+            assets[1] = IERC20(chainAddresses.lsd.SFRXETH_ADDRESS);
+            assets[2] = IERC20(chainAddresses.lsd.WOETH_ADDRESS);
+
+            strategies = new IStrategy[](2);
+            strategies[0] = IStrategy(chainAddresses.lsdStrategies.STETH_STRATEGY_ADDRESS);
+            strategies[1] = IStrategy(chainAddresses.lsdStrategies.SFRXETH_STRATEGY_ADDRESS);
+            strategies[2] = IStrategy(chainAddresses.lsdStrategies.OETH_STRATEGY_ADDRESS);
+
+        } else if (block.chainid == 17000) {
+            assets = new IERC20[](3);
+            assets[0] = IERC20(chainAddresses.lsd.WSTETH_ADDRESS);
+            assets[1] = IERC20(chainAddresses.lsd.SFRXETH_ADDRESS);
+            assets[2] = IERC20(chainAddresses.lsd.RETH_ADDRESS);
+            assets[3] = IERC20(chainAddresses.lsd.METH_ADDRESS);
+
+            strategies = new IStrategy[](3);
+            strategies[0] = IStrategy(chainAddresses.lsdStrategies.STETH_STRATEGY_ADDRESS);
+            strategies[1] = IStrategy(chainAddresses.lsdStrategies.SFRXETH_STRATEGY_ADDRESS);
+            strategies[2] = IStrategy(chainAddresses.lsdStrategies.RETH_STRATEGY_ADDRESS);
+            strategies[3] = IStrategy(chainAddresses.lsdStrategies.METH_STRATEGY_ADDRESS);
+        } else {
+            revert(string(string.concat("Chain ID ", vm.toString(block.chainid), " not supported")));
+        }
+
+        {
+            EigenStrategyManager eigenStrategyManagerImplementation = new EigenStrategyManager();
+            TransparentUpgradeableProxy eigenStrategyManagerProxy = new TransparentUpgradeableProxy(address(eigenStrategyManagerImplementation), actors.admin.PROXY_ADMIN_OWNER, "");
+            eigenStrategyManager = EigenStrategyManager(address(eigenStrategyManagerProxy));
+        }
+
+        {
+            TokenStakingNodesManager tokenStakingNodesManagerImplementation = new TokenStakingNodesManager();
+            TransparentUpgradeableProxy tokenStakingNodesManagerProxy = new TransparentUpgradeableProxy(address(tokenStakingNodesManagerImplementation), actors.admin.PROXY_ADMIN_OWNER, "");
+            tokenStakingNodesManager = TokenStakingNodesManager(address(tokenStakingNodesManagerProxy));
+        }
+
+        {
+            AssetRegistry assetRegistryImplementation = new AssetRegistry();
+            TransparentUpgradeableProxy assetRegistryProxy = new TransparentUpgradeableProxy(address(assetRegistryImplementation), actors.admin.PROXY_ADMIN_OWNER, "");
+            assetRegistry = AssetRegistry(address(assetRegistryProxy));
+        }
+
+        // Initialize ynLSDe
+        {
+            address[] memory lsdPauseWhitelist = new address[](0);
+
+            ynEigen.Init memory ynlsdeInit = ynEigen.Init({
+                name: "Eigenlayer YieldNest LSD",
+                symbol: "ynLSDe",
+                admin: actors.admin.ADMIN,
+                pauser: actors.ops.PAUSE_ADMIN,
+                unpauser: actors.admin.UNPAUSE_ADMIN,
+                eigenStrategyManager: IEigenStrategyManager(address(eigenStrategyManager)),
+                assetRegistry: IAssetRegistry(address(assetRegistry)),
+                pauseWhitelist: lsdPauseWhitelist
+            });
+            ynLSDe.initialize(ynlsdeInit);
+        }
+
+        {
+            AssetRegistry.Init memory assetRegistryInit = AssetRegistry.Init({
+                assets: assets,
+                rateProvider: IRateProvider(address(rateProvider)),
+                eigenStrategyManager: IEigenStrategyManager(address(eigenStrategyManager)),
+                ynEigen: IynEigen(address(ynLSDe)),
+                admin: actors.admin.ADMIN,
+                pauser: actors.ops.PAUSE_ADMIN,
+                unpauser: actors.admin.UNPAUSE_ADMIN,
+                assetManagerRole: actors.admin.ASSET_MANAGER
+            });
+            assetRegistry.initialize(assetRegistryInit);
+        }
+
+        {
+            EigenStrategyManager.Init memory eigenStrategyManagerInit = EigenStrategyManager.Init({
+                assets: assets,
+                strategies: strategies,
+                ynEigen: IynEigen(address(ynLSDe)),
+                strategyManager: IStrategyManager(address(eigenLayer.strategyManager)),
+                delegationManager: IDelegationManager(address(eigenLayer.delegationManager)),
+                tokenStakingNodesManager: ITokenStakingNodesManager(address(tokenStakingNodesManager)),
+                admin: actors.admin.ADMIN,
+                strategyController: actors.ops.STRATEGY_CONTROLLER,
+                unpauser: actors.admin.UNPAUSE_ADMIN,
+                pauser: actors.ops.PAUSE_ADMIN,
+                strategyAdmin: actors.admin.EIGEN_STRATEGY_ADMIN,
+                wstETH: IwstETH(chainAddresses.lsd.WSTETH_ADDRESS),
+                woETH: IERC4626(chainAddresses.lsd.WOETH_ADDRESS)
+            });
+            eigenStrategyManager.initialize(eigenStrategyManagerInit);
+        }
 
     //     {
     //         LSDStakingNode lsdStakingNodeImplementation = new LSDStakingNode();
@@ -137,5 +190,6 @@ contract DeployYnLSD {
             
     //         saveynLSDDeployment(deployment);
     //     }
-    // }
+    }
 }
+
