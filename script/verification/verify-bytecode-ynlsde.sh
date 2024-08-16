3b4fccb40e5bee94cbb52dbf65122c27762f9742
danoctavian <danoctavian91@gmail.com>
2024-08-17 02:32:10 +0300

    add mainnet checker

--- script/verification/verify-bytecode-ynlsde.sh
+++ script/verification/verify-bytecode-ynlsde.sh
@@ -4,7 +4,15 @@
 DEPLOYMENT_FILE="./deployments/YnLSDe-1.json"
 
 # List of contracts to verify
-contracts=("YnLSDe" "assetRegistry" "eigenStrategyManager" "tokenStakingNodesManager" "ynEigenDepositAdapter" "rateProvider" "ynEigenViewer")
+contracts=("YnLSDe" "assetRegistry" "eigenStrategyManager" "tokenStakingNodesManager" "ynEigenDepositAdapter" "rateProvider")
+
+# Note: verify "ynEigenViewer" manually. more difficult to craft parameter bytecode verification
+
+# Read the Etherscan API key from .env file
+ETHERSCAN_API_KEY=$(grep ETHERSCAN_API_KEY .env | cut -d '=' -f2 | tr -d '[:space:]')
+
+# Log ETHERSCAN_API_KEY (masked for security)
+echo "ETHERSCAN_API_KEY: ${ETHERSCAN_API_KEY:0:4}...${ETHERSCAN_API_KEY: -4}"
 
 
 get_solidity_contract_name() {
@@ -25,6 +33,10 @@ get_solidity_contract_name() {
         solidity_contract="LSDRateProvider"
     elif [ "$contract_key" == "ynEigenViewer" ]; then
         solidity_contract="ynEigenViewer"
+    elif [ "$contract_key" == "upgradeTimelock" ]; then
+        solidity_contract="TimelockController"
+    elif [ "$contract_key" == "tokenStakingNodeImplementation" ]; then
+        solidity_contract="TokenStakingNode"
     else
         echo "Error: Unknown contract key '$contract_key'" >&2
         return 1
@@ -40,6 +52,28 @@ extract_addresses() {
     local impl_address=$(jq -r ".[\"implementation-$key\"]" "$DEPLOYMENT_FILE")
     local proxy_admin=$(jq -r ".[\"proxyAdmin-$key\"]" "$DEPLOYMENT_FILE")
     echo "$proxy_address $impl_address $proxy_admin"
+}
+
+verify_implementation() {
+    local impl_address=$1
+    local solidity_contract=$2
+    local contract_name=$3
+
+    # Get bytecode from Etherscan
+    local etherscan_sha256=$(curl -X GET "https://api.etherscan.io/api?module=proxy&action=eth_getCode&address=$impl_address&tag=latest&apikey=$ETHERSCAN_API_KEY" | jq -r '.result' | sha256sum)
+
+    # Get local bytecode
+    local local_sha256=$(cat "out/$solidity_contract.sol/$solidity_contract.json" | jq -r '.deployedBytecode.object' | sed 's/"//g' | sha256sum)
+
+    # Compare SHA256 hashes
+    if [ "$etherscan_sha256" = "$local_sha256" ]; then
+        echo  "✅ Bytecode verification successful for $contract_name"
+    else
+        echo -e "❌ Error: Bytecode mismatch for $contract_name"
+        echo -e "❌ Etherscan SHA256: $etherscan_sha256"
+        echo -e "❌ Local SHA256: $local_sha256"
+        return 1
+    fi
 }
 
 # Function to verify a contract
@@ -58,28 +92,20 @@ verify_contract() {
 
     echo "Verifying $contract_name (Solidity contract: $solidity_contract)..."
 
-    # Get bytecode from Etherscan
-    local etherscan_bytecode=$(curl -s -X GET "https://api.etherscan.io/api?module=proxy&action=eth_getCode&address=$impl_address&tag=latest&apikey=$ETHERSCAN_API_KEY" | jq -r '.result')
-    
-    # Calculate SHA256 of Etherscan bytecode
-    local etherscan_sha256=$(echo -n "$etherscan_bytecode" | sed 's/0x//' | xxd -r -p | sha256sum | awk '{print $1}')
-
-    # Get local bytecode
-    local local_bytecode=$(cat "out/$solidity_contract.sol/$solidity_contract.json" | jq -r '.deployedBytecode.object')
-    
-    # Calculate SHA256 of local bytecode
-    local local_sha256=$(echo -n "$local_bytecode" | xxd -r -p | sha256sum | awk '{print $1}')
-
-    # Compare SHA256 hashes
-    if [ "$etherscan_sha256" = "$local_sha256" ]; then
-        echo "Bytecode verification successful for $contract_name"
-    else
-        echo "Error: Bytecode mismatch for $contract_name"
-        echo "Etherscan SHA256: $etherscan_sha256"
-        echo "Local SHA256: $local_sha256"
-        return 1
-    fi
+    verify_implementation "$impl_address" "$solidity_contract" "$contract_name"
 }
+
+# Extract upgradeTimelock address
+upgradeTimelock=$(jq -r '.upgradeTimelock' "$DEPLOYMENT_FILE")
+
+verify_contract "" $upgradeTimelock "" "upgradeTimelock"
+
+# Extract tokenStakingNodeImplementation address
+tokenStakingNodeImplementation=$(jq -r '.tokenStakingNodeImplementation' "$DEPLOYMENT_FILE")
+
+verify_contract "" $tokenStakingNodeImplementation "" "tokenStakingNodeImplementation"
+
+
 
 for contract in "${contracts[@]}"; do
     read proxy impl proxy_admin <<< $(extract_addresses $contract)
