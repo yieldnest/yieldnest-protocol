162d1f92e143f37ddfb117b71fc199f1b4d0e7ee
MrDeadCe11 <mister.deadce11@gmail.com>
2024-08-23 14:25:34 -0500

    base deployer, setting up bash script

---
+++ script/ynEigenDeployer/BaseDeployer.s.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: BSD 3-Clause License
+pragma solidity ^0.8.24;
+
+import {ContractAddresses} from "./addresses/ContractAddresses.sol";
+import {ActorAddresses} from "./addresses/Actors.sol";
+import {BaseYnEigenScript} from "script/BaseYnEigenScript.s.sol";
+import {BaseScript} from "script/BaseScript.s.sol";
+import "forge-std/Script.sol";
+
+import "forge-std/console.sol";
+
+struct Asset {
+    address addr;
+    string name;
+    address strategyAddres;
+}
+
+struct InputStruct {
+    Asset[] assets;
+    uint256 chainId;
+    address rateProvider;
+    string symbol;
+}
+
+contract BaseDeployer is Script, BaseScript, ContractAddresses, ActorAddresses {
+    using stdJson for string;
+
+    error IncorrectChainId(uint256 specifiedChainId, uint256 actualChainId);
+
+    InputStruct public inputs;
+
+    string public json;
+    string public path;
+
+    address internal _deployer;
+    uint256 internal _privateKey;
+    uint256 internal _chainId;
+    string internal _network;
+
+    function _loadState() internal {
+        _loadActorAddresses();
+        _loadContractAddresses();
+
+        if (block.chainid == 31_337) {
+            _privateKey = vm.envUint("ANVIL_ONE");
+            _deployer = vm.addr(_privateKey);
+        } else {
+            _privateKey = vm.envUint("PRIVATE_KEY");
+            _deployer = vm.addr(_privateKey);
+        }
+
+        console.log("\n");
+        console.log("Deployer address:", _deployer);
+        console.log("Deployer balance:", _deployer.balance);
+    }
+
+    function _loadJson(string memory _path) internal returns (string memory) {
+        string memory root = vm.projectRoot();
+        path = string(abi.encodePacked(root, _path));
+        json = vm.readFile(path);
+        return json;
+    }
+
+    function _loadJsonData() internal {
+        InputStruct memory _inputs;
+        bytes memory data = vm.parseJson(json);
+        _inputs = abi.decode(data, (InputStruct));
+        this.loadStructIntoMemory(_inputs);
+    }
+
+    /**
+     * @dev this function is required to load the JSON input struct into storage untill that feature is added to foundry
+     */
+    function loadStructIntoMemory(InputStruct calldata inputStruct) external {
+        inputs = inputStruct;
+    }
+
+    function _checkNetworkParams() internal virtual {
+        console.log("ChainId:", inputs.chainId);
+        if (block.chainid != inputs.chainId) revert IncorrectChainId(inputs.chainId, block.chainid);
+    }
+}
