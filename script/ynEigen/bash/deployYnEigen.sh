0cd27cb9ff08902b522f74ef78ba33ba096ae0f0
MrDeadCe11 <mister.deadce11@gmail.com>
2024-08-24 18:31:25 -0500

    finished bash script

--- script/ynEigen/bash/deployYnEigen.sh
+++ script/ynEigen/bash/deployYnEigen.sh
@@ -6,7 +6,7 @@ set -e
 ######################
 ## GLOBAL VARIABLES ##
 ######################
-echo "starting script"
+
 # Read the Etherscan API key from .env file
 ETHERSCAN_API_KEY=$(grep ETHERSCAN_API_KEY .env | cut -d '=' -f2)
 # Read the RPC URL from .env file
@@ -40,8 +40,7 @@ function simulate() {
 function broadcast() {
     forge script $1 -s $2 --rpc-url $3 --account yieldnestDeployerKey --sender $DEPLOYER_ADDRESS --broadcast --etherscan-api-key $ETHERSCAN_API_KEY --verify
 }
-
-function deploy() {
+function simulator() {
     # the first argument should be the path to the JSON input file
     INPUT_JSON=$1
     CHAIN=$(jq -r ".chainId" "$INPUT_JSON")
@@ -59,14 +58,55 @@ function deploy() {
     fi
 
     simulate script/ynEigen/YnEigenScript.s.sol:YnEigenScript $CALLDATA $INFURA_ADDRESS
+}
+function deploy() {
+    # the first argument should be the path to the JSON input file
+    INPUT_JSON=$1
+    CHAIN=$(jq -r ".chainId" "$INPUT_JSON")
+    CALLDATA=$(cast calldata "run(string)" "/$INPUT_JSON")
+    INFURA_ADDRESS=""
+
+    if [[ $CHAIN == 1 ]]; then
+        INFURA_ADDRESS=https://mainnet.infura.io/v3/$INFURA_PROJECT_ID
+    elif [[ $CHAIN == 17000 ]]; then
+        INFURA_ADDRESS=https://holesky.infura.io/v3/$INFURA_PROJECT_ID
+    elif [[ $CHAIN == 31337 ]]; then
+        INFURA_ADDRESS=http://127.0.0.1:8545
+    else
+        exit 1
+    fi
+
+    broadcast script/ynEigen/YnEigenScript.s.sol:YnEigenScript $CALLDATA $INFURA_ADDRESS
 
 }
 if [[ "$1" == "" ]]; then
     echo "$1"
     display_help
 else
-    echo "DEPLOYING..."
-    deploy $1
+    read -p "Would you like to simulate this transaction before deployment? y/n " CONFIRMATION
+
+    case $CONFIRMATION in
+    [Yy]*)
+        echo "Simulating..."
+        simulator $1
+        ;;
+    [Nn]*)
+        echo "Deploying..."
+        ;;
+    esac
+    delimitier
+    read -p "Would you like to continue deploying? y/n " DEPLOYMENT
+    case $DEPLOYMENT in
+    [Yy]*)
+        echo "Deploying..."
+        deploy $1
+        ;;
+    [Nn]*)
+        echo "Exiting."
+        exit 0
+        ;;
+    esac
+
 fi
 
 echo "script finished"
