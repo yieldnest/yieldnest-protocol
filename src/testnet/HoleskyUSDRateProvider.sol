ac3e25212af3680a01fa65d9e8910f267b1f5314
danoctavian <danoctavian91@gmail.com>
2024-08-29 02:35:09 +0300

    fix name for USD rate provider

--- src/testnet/HoleskyUSDRateProvider.sol
+++ src/testnet/HoleskyUSDRateProvider.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {IERC4626} from "lib/openzeppelin-contracts/contracts/interfaces/IERC4626.sol";
 
-contract HoleskyLSDRateProvider {
+contract HoleskyUSDRateProvider {
 
     //--------------------------------------------------------------------------------------
     //----------------------------------  ERRORS  ------------------------------------------
