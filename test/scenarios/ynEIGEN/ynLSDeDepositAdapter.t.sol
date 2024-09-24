fcf464ab18cf01e57c34984940303f2e0701f258
johnnyonline <johnnyonline.eth@gmail.com>
2024-09-24 14:10:59 +0900

    chore: remove console.log

--- test/scenarios/ynEIGEN/ynLSDeDepositAdapter.t.sol
+++ test/scenarios/ynEIGEN/ynLSDeDepositAdapter.t.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.24;
 import {TestAssetUtils} from "test/utils/TestAssetUtils.sol";
 
 import "./ynLSDeWithdrawals.t.sol";
-import "forge-std/console.sol";
 
 contract ynLSDeDepositAdapterTest is ynLSDeWithdrawalsTest {
 
@@ -48,7 +47,6 @@ contract ynLSDeDepositAdapterTest is ynLSDeWithdrawalsTest {
 
         vm.startPrank(user);
         IERC20(chainAddresses.lsd.OETH_ADDRESS).approve(address(ynEigenDepositAdapter_), _amount);
-        console.log("OETH balance before deposit: ", IERC20(chainAddresses.lsd.OETH_ADDRESS).balanceOf(user));
         uint256 _ynOut = ynEigenDepositAdapter_.deposit(IERC20(chainAddresses.lsd.OETH_ADDRESS), _amount, user);
         vm.stopPrank();
 
