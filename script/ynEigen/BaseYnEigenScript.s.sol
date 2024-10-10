fb740823070019069c208959ed241832334ba3db
johnnyonline <johnnyonline.eth@gmail.com>
2024-10-10 14:50:13 +0900

    feat: ynFoo deployment script

--- script/ynEigen/BaseYnEigenScript.s.sol
+++ script/ynEigen/BaseYnEigenScript.s.sol
@@ -10,6 +10,9 @@ import {TokenStakingNode} from "src/ynEIGEN/TokenStakingNode.sol";
 import {ynEigenDepositAdapter} from "src/ynEIGEN/ynEigenDepositAdapter.sol";
 import {IRateProvider} from "src/interfaces/IRateProvider.sol";
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+import {LSDWrapper} from "src/ynEIGEN/LSDWrapper.sol";
+import {RedemptionAssetsVault} from "src/ynEIGEN/RedemptionAssetsVault.sol";
+import {WithdrawalQueueManager} from "src/WithdrawalQueueManager.sol";
 
 import {ActorAddresses} from "script/Actors.sol";
 import {ContractAddresses} from "script/ContractAddresses.sol";
@@ -31,6 +34,9 @@ contract BaseYnEigenScript is BaseScript {
         IRateProvider rateProvider;
         TimelockController upgradeTimelock;
         ynEigenViewer viewer;
+        RedemptionAssetsVault redemptionAssetsVault;
+        WithdrawalQueueManager withdrawalQueueManager;
+        LSDWrapper lsdWrapper;
     }
 
     struct Asset {
@@ -110,6 +116,10 @@ contract BaseYnEigenScript is BaseScript {
         serializeProxyElements(json, "rateProvider", address(deployment.rateProvider));
         serializeProxyElements(json, "ynEigenViewer", address(deployment.viewer));
         vm.serializeAddress(json, "upgradeTimelock", address(deployment.upgradeTimelock));
+        serializeProxyElements(json, "redemptionAssetsVault", address(deployment.redemptionAssetsVault));
+        serializeProxyElements(json, "withdrawalQueueManager", address(deployment.withdrawalQueueManager));
+        serializeProxyElements(json, "lsdWrapper", address(deployment.lsdWrapper));
+
 
         // actors
         vm.serializeAddress(json, "PROXY_ADMIN_OWNER", address(actors.admin.PROXY_ADMIN_OWNER));
--- script/ynEigen/YnEigenFactory.sol
+++ script/ynEigen/YnEigenFactory.sol
@@ -7,7 +7,7 @@ import {IDelegationManager} from "lib/eigenlayer-contracts/src/contracts/interfa
 import {IStrategyManager} from "lib/eigenlayer-contracts/src/contracts/interfaces/IStrategyManager.sol";
 
 import {ITokenStakingNodesManager} from "src/interfaces/ITokenStakingNodesManager.sol";
-
+import {IRedeemableAsset} from "src/interfaces/IRedeemableAsset.sol";
 import {IynEigen} from "src/interfaces/IynEigen.sol";
 import {IRateProvider} from "src/interfaces/IRateProvider.sol";
 import {IAssetRegistry} from "src/interfaces/IAssetRegistry.sol";
@@ -21,7 +21,10 @@ import {TokenStakingNode} from "src/ynEIGEN/TokenStakingNode.sol";
 import {AssetRegistry} from "src/ynEIGEN/AssetRegistry.sol";
 import {ynEigenDepositAdapter} from "src/ynEIGEN/ynEigenDepositAdapter.sol";
 import {ynEigenViewer} from "src/ynEIGEN/ynEigenViewer.sol";
-import {YnEigenInit} from "./YnEigenStructs.sol";
+import {YnEigenInit, YnEigenProxies} from "./YnEigenStructs.sol";
+import {LSDWrapper} from "src/ynEIGEN/LSDWrapper.sol";
+import {RedemptionAssetsVault} from "src/ynEIGEN/RedemptionAssetsVault.sol";
+import {WithdrawalQueueManager} from "src/WithdrawalQueueManager.sol";
 
 import {IwstETH} from "src/external/lido/IwstETH.sol";
 import {IERC4626} from "lib/openzeppelin-contracts/contracts/interfaces/IERC4626.sol";
@@ -47,56 +50,44 @@ contract YnEigenFactory is IYnEigenFactory {
         proxy = address(new TransparentUpgradeableProxy(implementation, controller, ""));
     }
 
-    function deploy(YnEigenInit calldata init)
-        external
-        returns (
-            ynEigen ynToken,
-            EigenStrategyManager eigenStrategyManager,
-            TokenStakingNodesManager tokenStakingNodesManager,
-            TokenStakingNode tokenStakingNode,
-            AssetRegistry assetRegistry,
-            ynEigenDepositAdapter ynEigenDepositAdapterInstance,
-            IRateProvider rateProvider,
-            TimelockController timelock,
-            ynEigenViewer viewer
-        )
-    {
+    function deploy(YnEigenInit calldata init) external returns (YnEigenProxies memory _proxies) {
+
         // Deploy proxies
-        ynToken = ynEigen(_deployProxy(init.implementations.ynEigen, init.timelock));
-        eigenStrategyManager =
-            EigenStrategyManager(_deployProxy(init.implementations.eigenStrategyManager, init.timelock));
-        tokenStakingNodesManager =
-            TokenStakingNodesManager(_deployProxy(init.implementations.tokenStakingNodesManager, init.timelock));
-        tokenStakingNode = TokenStakingNode(init.implementations.tokenStakingNode);
-        assetRegistry = AssetRegistry(_deployProxy(init.implementations.assetRegistry, init.timelock));
-        ynEigenDepositAdapterInstance =
-            ynEigenDepositAdapter(_deployProxy(init.implementations.depositAdapter, init.timelock));
-        rateProvider = IRateProvider(_deployProxy(init.implementations.rateProvider, init.timelock));
-        timelock = TimelockController(payable(init.timelock));
+        _proxies.ynToken = ynEigen(_deployProxy(init.implementations.ynEigen, init.timelock));
+        _proxies.eigenStrategyManager = EigenStrategyManager(_deployProxy(init.implementations.eigenStrategyManager, init.timelock));
+        _proxies.tokenStakingNodesManager = TokenStakingNodesManager(_deployProxy(init.implementations.tokenStakingNodesManager, init.timelock));
+        _proxies.tokenStakingNode = TokenStakingNode(init.implementations.tokenStakingNode);
+        _proxies.assetRegistry = AssetRegistry(_deployProxy(init.implementations.assetRegistry, init.timelock));
+        _proxies.ynEigenDepositAdapterInstance = ynEigenDepositAdapter(_deployProxy(init.implementations.depositAdapter, init.timelock));
+        _proxies.rateProvider = IRateProvider(_deployProxy(init.implementations.rateProvider, init.timelock));
+        _proxies.redemptionAssetsVault = RedemptionAssetsVault(_deployProxy(init.implementations.redemptionAssetsVault, init.timelock));
+        _proxies.withdrawalQueueManager = WithdrawalQueueManager(_deployProxy(init.implementations.withdrawalQueueManager, init.timelock));
+        _proxies.lsdWrapper = LSDWrapper(_deployProxy(init.implementations.lsdWrapper, init.timelock));
+        _proxies.timelock = TimelockController(payable(init.timelock));
 
         // Initialize ynToken
-        ynToken.initialize(
+        _proxies.ynToken.initialize(
             ynEigen.Init({
                 name: init.name,
                 symbol: init.symbol,
                 admin: init.actors.ADMIN,
                 pauser: init.actors.PAUSE_ADMIN,
                 unpauser: init.actors.UNPAUSE_ADMIN,
-                yieldNestStrategyManager: address(eigenStrategyManager),
-                assetRegistry: IAssetRegistry(address(assetRegistry)),
+                yieldNestStrategyManager: address(_proxies.eigenStrategyManager),
+                assetRegistry: IAssetRegistry(address(_proxies.assetRegistry)),
                 pauseWhitelist: new address[](0)
             })
         );
 
         // Initialize eigenStrategyManager
-        eigenStrategyManager.initialize(
+        _proxies.eigenStrategyManager.initialize(
             EigenStrategyManager.Init({
                 assets: init.assets,
                 strategies: init.strategies,
-                ynEigen: IynEigen(address(ynToken)),
+                ynEigen: IynEigen(address(_proxies.ynToken)),
                 strategyManager: IStrategyManager(init.chainAddresses.STRATEGY_MANAGER),
                 delegationManager: IDelegationManager(init.chainAddresses.DELEGATION_MANAGER),
-                tokenStakingNodesManager: ITokenStakingNodesManager(address(tokenStakingNodesManager)),
+                tokenStakingNodesManager: ITokenStakingNodesManager(address(_proxies.tokenStakingNodesManager)),
                 admin: init.actors.ADMIN,
                 pauser: init.actors.PAUSE_ADMIN,
                 unpauser: init.actors.UNPAUSE_ADMIN,
@@ -108,12 +99,12 @@ contract YnEigenFactory is IYnEigenFactory {
         );
 
         // Initialize assetRegistry
-        assetRegistry.initialize(
+        _proxies.assetRegistry.initialize(
             AssetRegistry.Init({
                 assets: init.assets,
-                rateProvider: IRateProvider(address(rateProvider)),
-                yieldNestStrategyManager: IYieldNestStrategyManager(address(eigenStrategyManager)),
-                ynEigen: IynEigen(address(ynToken)),
+                rateProvider: IRateProvider(address(_proxies.rateProvider)),
+                yieldNestStrategyManager: IYieldNestStrategyManager(address(_proxies.eigenStrategyManager)),
+                ynEigen: IynEigen(address(_proxies.ynToken)),
                 admin: init.actors.ADMIN,
                 pauser: init.actors.PAUSE_ADMIN,
                 unpauser: init.actors.UNPAUSE_ADMIN,
@@ -122,13 +113,13 @@ contract YnEigenFactory is IYnEigenFactory {
         );
 
         // Initialize tokenStakingNodesManager
-        tokenStakingNodesManager.initialize(
+        _proxies.tokenStakingNodesManager.initialize(
             TokenStakingNodesManager.Init({
                 admin: address(this), // Placeholder; changed post tokenStakingNode registration
                 stakingAdmin: address(this), // Placeholder; changed post tokenStakingNode registration
                 strategyManager: IStrategyManager(init.chainAddresses.STRATEGY_MANAGER),
                 delegationManager: IDelegationManager(init.chainAddresses.DELEGATION_MANAGER),
-                yieldNestStrategyManager: address(eigenStrategyManager),
+                yieldNestStrategyManager: address(_proxies.eigenStrategyManager),
                 maxNodeCount: init.maxNodeCount,
                 pauser: init.actors.PAUSE_ADMIN,
                 unpauser: init.actors.UNPAUSE_ADMIN,
@@ -138,19 +129,53 @@ contract YnEigenFactory is IYnEigenFactory {
             })
         );
 
+        // initialize eigenStrategyManager
+        {
+            _proxies.eigenStrategyManager.initializeV2(address(_proxies.redemptionAssetsVault), address(_proxies.lsdWrapper), init.actors.ADMIN);
+        }
+
+        // initialize RedemptionAssetsVault
+        {
+            RedemptionAssetsVault.Init memory _init = RedemptionAssetsVault.Init({
+                admin: init.actors.ADMIN,
+                redeemer: address(_proxies.withdrawalQueueManager),
+                ynEigen: _proxies.ynToken,
+                assetRegistry: _proxies.assetRegistry
+            });
+            _proxies.redemptionAssetsVault.initialize(_init);
+        }
+
+        // initialize WithdrawalQueueManager
+        {
+            WithdrawalQueueManager.Init memory _init = WithdrawalQueueManager.Init({
+                name: "ynLSDe Withdrawal Manager",
+                symbol: "ynLSDeWM",
+                redeemableAsset: IRedeemableAsset(address(_proxies.ynToken)),
+                redemptionAssetsVault: _proxies.redemptionAssetsVault,
+                admin: init.actors.ADMIN,
+                withdrawalQueueAdmin: init.actors.ADMIN,
+                redemptionAssetWithdrawer: init.actors.ADMIN,
+                requestFinalizer:  init.actors.ADMIN,
+                // withdrawalFee: 500, // 0.05%
+                withdrawalFee: 0,
+                feeReceiver: init.actors.ADMIN
+            });
+            _proxies.withdrawalQueueManager.initialize(_init);
+        }
+
         // Register tokenStakingNode
-        tokenStakingNodesManager.registerTokenStakingNode(address(tokenStakingNode));
+        _proxies.tokenStakingNodesManager.registerTokenStakingNode(address(_proxies.tokenStakingNode));
 
         // Set roles post tokenStakingNode registration
-        tokenStakingNodesManager.grantRole(tokenStakingNodesManager.DEFAULT_ADMIN_ROLE(), init.actors.ADMIN);
-        tokenStakingNodesManager.grantRole(tokenStakingNodesManager.STAKING_ADMIN_ROLE(), init.timelock);
-        tokenStakingNodesManager.revokeRole(tokenStakingNodesManager.STAKING_ADMIN_ROLE(), address(this));
-        tokenStakingNodesManager.revokeRole(tokenStakingNodesManager.DEFAULT_ADMIN_ROLE(), address(this));
+        _proxies.tokenStakingNodesManager.grantRole(_proxies.tokenStakingNodesManager.DEFAULT_ADMIN_ROLE(), init.actors.ADMIN);
+        _proxies.tokenStakingNodesManager.grantRole(_proxies.tokenStakingNodesManager.STAKING_ADMIN_ROLE(), init.timelock);
+        _proxies.tokenStakingNodesManager.revokeRole(_proxies.tokenStakingNodesManager.STAKING_ADMIN_ROLE(), address(this));
+        _proxies.tokenStakingNodesManager.revokeRole(_proxies.tokenStakingNodesManager.DEFAULT_ADMIN_ROLE(), address(this));
 
         // ynEigenDepositAdapter
-        ynEigenDepositAdapterInstance.initialize(
+        _proxies.ynEigenDepositAdapterInstance.initialize(
             ynEigenDepositAdapter.Init({
-                ynEigen: address(ynToken),
+                ynEigen: address(_proxies.ynToken),
                 wstETH: init.chainAddresses.WSTETH_ADDRESS,
                 woETH: init.chainAddresses.WOETH_ADDRESS,
                 admin: init.actors.ADMIN
@@ -160,21 +185,21 @@ contract YnEigenFactory is IYnEigenFactory {
         // ynEigenViewer
         {
             ynEigenViewer viewerImplementation = new ynEigenViewer(
-                address(assetRegistry), address(ynToken), address(tokenStakingNodesManager), address(rateProvider)
+                address(_proxies.assetRegistry), address(_proxies.ynToken), address(_proxies.tokenStakingNodesManager), address(_proxies.rateProvider)
             );
-            viewer = ynEigenViewer(_deployProxy(address(viewerImplementation), init.actors.YN_SECURITY_COUNCIL));
+            _proxies.viewer = ynEigenViewer(_deployProxy(address(viewerImplementation), init.actors.YN_SECURITY_COUNCIL));
         }
 
         emit YnEigenDeployed(
-            ynToken,
-            eigenStrategyManager,
-            tokenStakingNodesManager,
-            tokenStakingNode,
-            assetRegistry,
-            ynEigenDepositAdapterInstance,
-            rateProvider,
-            timelock,
-            viewer
+            _proxies.ynToken,
+            _proxies.eigenStrategyManager,
+            _proxies.tokenStakingNodesManager,
+            _proxies.tokenStakingNode,
+            _proxies.assetRegistry,
+            _proxies.ynEigenDepositAdapterInstance,
+            _proxies.rateProvider,
+            _proxies.timelock,
+            _proxies.viewer
         );
     }
 }
