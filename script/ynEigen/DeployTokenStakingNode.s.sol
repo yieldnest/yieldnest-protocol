213d1895b27c4317fb8fe0b185cfa06eef46df45
yield-farmer-1 <parth@yieldnest.finance>
2025-02-05 11:03:54 +0530

    add deployment script and deployed address

---
+++ script/ynEigen/DeployTokenStakingNode.s.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: BSD 3-Clause License
+pragma solidity ^0.8.24;
+
+import {BaseYnEigenScript} from "script/ynEigen/BaseYnEigenScript.s.sol";
+import {TokenStakingNode} from "src/ynEIGEN/TokenStakingNode.sol";
+import {console} from "lib/forge-std/src/console.sol";
+
+contract DeployTokenStakingNode is BaseYnEigenScript {
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        address publicKey = vm.addr(deployerPrivateKey);
+        console.log("Deployer Public Key:", publicKey);
+
+        address _broadcaster = vm.addr(deployerPrivateKey);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        console.log("Current Block Number:", block.number);
+        console.log("Current Chain ID:", block.chainid);
+
+        TokenStakingNode tokenStakingNodeImplementation = new TokenStakingNode();
+
+        console.log("TokenStakingNode Implementation:", address(tokenStakingNodeImplementation));
+
+        // Deployment memory deployment = loadDeployment();
+
+        // deployment.tokenStakingNodeImplementation = tokenStakingNodeImplementation;
+
+        // saveDeployment(deployment);
+
+        vm.stopBroadcast();
+    }
+
+}
+
+// == Logs ==
+//   Deployer Public Key: 0x445b64828683ae4B6D5f0542f9E97707d631A847
+//   TokenStakingNode Implementation: 0x28fD4C04620D95c4BeC9397E2D303937779C41FD
