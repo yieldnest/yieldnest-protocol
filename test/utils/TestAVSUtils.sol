dd11186d6d3571c0d8a5f9be0bd7db45db8fe6f2
danoctavian <danoctavian91@gmail.com>
2025-04-16 01:33:13 +0300

    fix TestAVSutils

--- test/utils/TestAVSUtils.sol
+++ test/utils/TestAVSUtils.sol
@@ -11,8 +11,9 @@ import {MockAVSRegistrar} from "lib/eigenlayer-contracts/src/test/mocks/MockAVSR
 import {IAllocationManagerTypes} from "lib/eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
 import {AllocationManagerStorage} from "lib/eigenlayer-contracts/src/contracts/core/AllocationManagerStorage.sol";
 import {OperatorSet} from "lib/eigenlayer-contracts/src/contracts/libraries/OperatorSetLib.sol";
-
-
+import {Vm} from "forge-std/Vm.sol";
+import {IDelegationManager} from "lib/eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
+import {IAllocationManager} from "lib/eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
 
 contract TestAVSUtils  {
 
@@ -25,34 +26,48 @@ contract TestAVSUtils  {
         IStrategy[] strategies;
         uint32 operatorSetId;
         string metadataURI;
+        uint64[] magnitudes;
     }
-
     /**
      * @notice Sets up an AVS, registers an operator, and allocates shares
      * @param vm The forge VM instance
-     * @param eigenLayer The EigenLayer instance
-     * @param params The parameters for AVS setup
+     * @param delegationManager The EigenLayer delegation manager
+     * @param allocationManager The EigenLayer allocation manager
+     * @param params The parameters for AVS setup including:
+     *        - avs: Address of the AVS
+     *        - operator: Address of the operator to register
+     *        - delegator: Address of the delegator
+     *        - tokenStakingNode: The token staking node contract
+     *        - strategies: Array of strategies to allocate
+     *        - operatorSetId: ID for the operator set
+     *        - metadataURI: Metadata URI for the AVS
+     *        - magnitudes: Allocation magnitudes for each strategy
      */
     function setupAVSAndRegisterOperator(
         Vm vm,
-        IEigenLayerContracts eigenLayer,
+        IDelegationManager delegationManager,
+        IAllocationManager allocationManager,
         AVSSetupParams memory params
     ) public {
+        // Create AVS
+        address avs = address(new MockAVSRegistrar());
         // Update metadata URI for the AVS
         vm.prank(params.avs);
-        eigenLayer.allocationManager.updateAVSMetadataURI(params.avs, params.metadataURI);
+        allocationManager.updateAVSMetadataURI(params.avs, params.metadataURI);
 
         // Register operator if not already registered
-        if (!eigenLayer.delegationManager.isOperator(params.operator)) {
+        if (!delegationManager.isOperator(params.operator)) {
             vm.prank(params.operator);
-            eigenLayer.delegationManager.registerAsOperator(address(0), 0, "ipfs://some-ipfs-hash");
+            delegationManager.registerAsOperator(address(0), 0, "ipfs://some-ipfs-hash");
         }
 
-        // Delegate to operator
-        ISignatureUtilsMixinTypes.SignatureWithExpiry memory signature;
-        bytes32 approverSalt;
-        vm.prank(params.delegator);
-        params.tokenStakingNode.delegate(params.operator, signature, approverSalt);
+        //Delegate to operator
+        if (params.delegator != address(0) && params.tokenStakingNode != ITokenStakingNode(address(0))) {
+            ISignatureUtilsMixinTypes.SignatureWithExpiry memory signature;
+            bytes32 approverSalt;
+            vm.prank(params.delegator);
+            params.tokenStakingNode.delegate(params.operator, signature, approverSalt);
+        }
 
         // Create operator sets for the AVS
         IAllocationManagerTypes.CreateSetParams[] memory createSetParams = new IAllocationManagerTypes.CreateSetParams[](1);
@@ -61,87 +76,67 @@ contract TestAVSUtils  {
             strategies: params.strategies
         });
         
-        vm.prank(params.avs);
-        eigenLayer.allocationManager.createOperatorSets(params.avs, createSetParams);
+        vm.prank(avs);
+        allocationManager.createOperatorSets(params.avs, createSetParams);
 
         // Register for operator set
         IAllocationManagerTypes.RegisterParams memory registerParams = IAllocationManagerTypes.RegisterParams({
-            avs: params.avs,
+            avs: avs,
             operatorSetIds: new uint32[](1),
             data: new bytes(0)
         });
         registerParams.operatorSetIds[0] = params.operatorSetId;
         vm.prank(params.operator);
-        eigenLayer.allocationManager.registerForOperatorSets(params.operator, registerParams);
-    }
+        allocationManager.registerForOperatorSets(params.operator, registerParams);
 
-    /**
-     * @notice Allocates shares for an operator to an AVS
-     * @param vm The forge VM instance
-     * @param eigenLayer The EigenLayer instance
-     * @param operator The operator address
-     * @param avs The AVS address
-     * @param operatorSetId The operator set ID
-     * @param strategies The strategies to allocate
-     * @param magnitudes The magnitudes to allocate for each strategy
-     */
-    function allocateShares(
-        Vm vm,
-        IEigenLayerContracts eigenLayer,
-        address operator,
-        address avs,
-        uint32 operatorSetId,
-        IStrategy[] memory strategies,
-        uint64[] memory magnitudes
-    ) public {
-        require(strategies.length == magnitudes.length, "Strategies and magnitudes length mismatch");
+        // Wait for allocation delay to pass
+        vm.roll(block.number +  AllocationManagerStorage(address(allocationManager)).ALLOCATION_CONFIGURATION_DELAY() + 1);
         
+        // Allocate shares to make them slashable
         IAllocationManagerTypes.AllocateParams[] memory allocateParams = new IAllocationManagerTypes.AllocateParams[](1);
         allocateParams[0] = IAllocationManagerTypes.AllocateParams({
             operatorSet: OperatorSet({
                 avs: avs,
-                id: operatorSetId
+                id: params.operatorSetId
             }),
-            strategies: strategies,
-            newMagnitudes: magnitudes
+            strategies: params.strategies,
+            newMagnitudes: params.magnitudes
         });
         
-        vm.prank(operator);
-        eigenLayer.allocationManager.modifyAllocations(operator, allocateParams);
+        vm.prank(params.operator);
+        allocationManager.modifyAllocations(params.operator, allocateParams);
     }
 
     /**
      * @notice Slashes an operator for a specific strategy
      * @param vm The forge VM instance
-     * @param eigenLayer The EigenLayer instance
+     * @param allocationManager The AllocationManager instance
      * @param avs The AVS address
      * @param operator The operator address
      * @param operatorSetId The operator set ID
      * @param strategy The strategy to slash
      * @param wadsToSlash The amount to slash in WAD (18 decimals)
-     * @param description The description of the slashing
      */
     function slashOperator(
         Vm vm,
-        IEigenLayerContracts eigenLayer,
+        IAllocationManager allocationManager,
         address avs,
         address operator,
         uint32 operatorSetId,
         IStrategy strategy,
-        uint256 wadsToSlash,
-        string memory description
+        uint256 wadsToSlash
     ) public {
         IAllocationManagerTypes.SlashingParams memory slashingParams = IAllocationManagerTypes.SlashingParams({
             operator: operator,
             operatorSetId: operatorSetId,
             strategies: new IStrategy[](1),
             wadsToSlash: new uint256[](1),
-            description: description
+            description: "test"
         });
         slashingParams.strategies[0] = strategy;
         slashingParams.wadsToSlash[0] = wadsToSlash;
         
         vm.prank(avs);
-        eigenLayer.allocationManager.slashOperator(avs, slashingParams);
+        allocationManager.slashOperator(avs, slashingParams);
     }
 }
